545d482ebdccd2ebc386714c76f4f663507cbee6
Jacopo Rota <jacopo.r00ta@gmail.com>
2021-09-02 13:46:03 +0200

    MGDOBR1-5: add clean database comment in tests

--- manager/src/test/java/com/redhat/developer/manager/utils/DatabaseManagerUtils.java
+++ manager/src/test/java/com/redhat/developer/manager/utils/DatabaseManagerUtils.java
@@ -11,6 +11,12 @@ public class DatabaseManagerUtils {
     @Inject
     ConnectorDAO connectorDAO;
 
+    /**
+     *   In this method there should be all the DAOs so to clean up the database entirely.
+     *
+     *   This is needed because we have tests with restassured + data stored in the database and @TestTransaction does not
+     *   rollback the transaction on server side: the database must be cleaned up manually. See https://github.com/quarkusio/quarkus/issues/15436
+     */
     public void cleanDatabase() {
         connectorDAO.deleteAll();
     }
