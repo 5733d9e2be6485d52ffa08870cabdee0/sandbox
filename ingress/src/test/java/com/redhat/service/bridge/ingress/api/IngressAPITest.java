d0cd6cb150427b6dfb5d5f97ea76cf5ca5b2e8ae
Jacopo Rota <jacopo.r00ta@gmail.com>
2021-10-11 14:39:01 +0200

    MGDOBR-54: enable support for non-cloud events

--- ingress/src/test/java/com/redhat/service/bridge/ingress/api/IngressAPITest.java
+++ ingress/src/test/java/com/redhat/service/bridge/ingress/api/IngressAPITest.java
@@ -2,7 +2,9 @@ package com.redhat.service.bridge.ingress.api;
 
 import javax.inject.Inject;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -18,6 +20,8 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 
 import static io.restassured.RestAssured.given;
 import static org.mockito.ArgumentMatchers.any;
@@ -27,6 +31,13 @@ import static org.mockito.Mockito.verify;
 
 @QuarkusTest
 public class IngressAPITest {
+
+    private static final String TOPIC_NAME = "topicName";
+    private static final String HEADER_CE_SPECVERSION = "1.0";
+    private static final String HEADER_CE_ID = "myId";
+    private static final String HEADER_CE_TYPE = "myId";
+    private static final String HEADER_CE_SOURCE = "mySource";
+    private static final String HEADER_CE_SUBJECT = "mySubject";
 
     @InjectMock
     KafkaEventPublisher kafkaEventPublisher;
@@ -41,30 +52,68 @@ public class IngressAPITest {
         QuarkusMock.installMockForType(mock, KafkaEventPublisher.class);
     }
 
+    @BeforeEach
+    public void init() {
+        ingressService.deploy(TOPIC_NAME);
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        ingressService.undeploy(TOPIC_NAME);
+    }
+
     @Test
     public void testSendCloudEvent() throws JsonProcessingException {
-        doApiCallAfterDeploy(TestUtils.buildTestCloudEvent(), 200);
-        verify(kafkaEventPublisher, times(1)).sendEvent(eq("topicName"), any(CloudEvent.class));
+        doApiCall(TestUtils.buildTestCloudEvent(), 200);
+        verify(kafkaEventPublisher, times(1)).sendEvent(eq(TOPIC_NAME), any(CloudEvent.class));
     }
 
     @Test
     public void testSendCloudEventWithBadRequestException() throws JsonProcessingException {
         Mockito.doCallRealMethod().when(kafkaEventPublisher).sendEvent(any(String.class), any(CloudEvent.class));
-        doApiCallAfterDeploy(TestUtils.buildTestCloudEventWithReservedAttributes(), 400);
-        verify(kafkaEventPublisher, times(1)).sendEvent(eq("topicName"), any(CloudEvent.class));
+        doApiCall(TestUtils.buildTestCloudEventWithReservedAttributes(), 400);
+        verify(kafkaEventPublisher, times(1)).sendEvent(eq(TOPIC_NAME), any(CloudEvent.class));
     }
 
     @Test
     public void testNonCloudEvent() {
-        doApiCallAfterDeploy("{\"key\": \"not a cloud event\"}", 400);
-        verify(kafkaEventPublisher, times(0)).sendEvent(eq("topicName"), any(CloudEvent.class));
+        doApiCall("{\"key\": \"not a cloud event\"}", 400);
+        verify(kafkaEventPublisher, times(0)).sendEvent(eq(TOPIC_NAME), any(CloudEvent.class));
+    }
+
+    @Test
+    public void testPlainEndpointWithoutHeaders() {
+        doPlainApiCall("{\"key\": \"value\"}", new Headers(), 400);
+        verify(kafkaEventPublisher, times(0)).sendEvent(eq(TOPIC_NAME), any(CloudEvent.class));
+    }
+
+    @Test
+    public void testPlainEndpoint() {
+        Headers headers = buildHeaders(HEADER_CE_SPECVERSION, HEADER_CE_TYPE, HEADER_CE_ID, HEADER_CE_SOURCE, HEADER_CE_SUBJECT);
+        doPlainApiCall("{\"key\": \"value\"}", headers, 200);
+        verify(kafkaEventPublisher, times(1)).sendEvent(eq(TOPIC_NAME), any(CloudEvent.class));
+    }
+
+    @Test
+    public void testPlainEndpointWithInvalidCloudEventSpecVersion() {
+        Headers headers = buildHeaders("not-a-valid-specversion", HEADER_CE_TYPE, HEADER_CE_ID, HEADER_CE_SOURCE, HEADER_CE_SUBJECT);
+        doPlainApiCall("{\"key\": \"value\"}", headers, 400);
+        verify(kafkaEventPublisher, times(0)).sendEvent(eq(TOPIC_NAME), any(CloudEvent.class));
+    }
+
+    @Test
+    public void testPlainEndpointWithInvalidURI() {
+        Headers headers = buildHeaders(HEADER_CE_SPECVERSION, HEADER_CE_TYPE, HEADER_CE_ID, "{not-a-valid-source}", HEADER_CE_SUBJECT);
+        doPlainApiCall("{\"key\": \"value\"}", headers, 400);
+        verify(kafkaEventPublisher, times(0)).sendEvent(eq(TOPIC_NAME), any(CloudEvent.class));
     }
 
     @Test
     // TODO: remove after we move to k8s
     public void testSendCloudEventToUndeployedInstance() throws JsonProcessingException {
+        ingressService.undeploy(TOPIC_NAME);
         doApiCall(TestUtils.buildTestCloudEvent(), 500);
-        verify(kafkaEventPublisher, times(0)).sendEvent(eq("topicName"), any(CloudEvent.class));
+        verify(kafkaEventPublisher, times(0)).sendEvent(eq(TOPIC_NAME), any(CloudEvent.class));
     }
 
     private void doApiCall(CloudEvent bodyEvent, int expectedStatusCode) {
@@ -77,17 +126,27 @@ public class IngressAPITest {
                 .contentType(ContentType.JSON)
                 .when()
                 .body(body)
-                .post("/ingress/events/topicName")
+                .post("/ingress/events/" + TOPIC_NAME)
                 .then().statusCode(expectedStatusCode);
     }
 
-    private void doApiCallAfterDeploy(CloudEvent bodyEvent, int expectedStatusCode) {
-        doApiCallAfterDeploy(CloudEventUtils.encode(bodyEvent), expectedStatusCode);
+    private void doPlainApiCall(String body, Headers headers, int expectedStatusCode) {
+        given()
+                .filter(new ResponseLoggingFilter())
+                .contentType(ContentType.JSON)
+                .headers(headers)
+                .when()
+                .body(body)
+                .post("/ingress/events/" + TOPIC_NAME + "/plain")
+                .then().statusCode(expectedStatusCode);
     }
 
-    private void doApiCallAfterDeploy(String body, int expectedStatusCode) {
-        ingressService.deploy("topicName");
-        doApiCall(body, expectedStatusCode);
-        ingressService.undeploy("topicName");
+    private Headers buildHeaders(String specVersion, String type, String id, String source, String subject) {
+        return new Headers(
+                new Header("ce-specversion", specVersion),
+                new Header("ce-type", type),
+                new Header("ce-id", id),
+                new Header("ce-source", source),
+                new Header("ce-subject", subject));
     }
 }
