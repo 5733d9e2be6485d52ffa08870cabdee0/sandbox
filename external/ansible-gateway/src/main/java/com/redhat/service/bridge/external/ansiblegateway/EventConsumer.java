8ec6b57f758a661b8fd0b97aab2e97b51dcb3ed5
Alessandro Costa <alecosta.me@gmail.com>
2021-10-13 11:51:43 +0200

    MGDOBR-63: add base ansible-gateway project

---
+++ external/ansible-gateway/src/main/java/com/redhat/service/bridge/external/ansiblegateway/EventConsumer.java
@@ -0,0 +1,47 @@
+package com.redhat.service.bridge.external.ansiblegateway;
+
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cloudevents.CloudEvent;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ApplicationScoped
+public class EventConsumer {
+
+    public static final String EVENTS_IN_TOPIC = "events-in";
+
+    private static final Logger LOG = LoggerFactory.getLogger(EventConsumer.class);
+
+    @Inject
+    ObjectMapper mapper;
+
+    @Incoming(EVENTS_IN_TOPIC)
+    public CompletionStage<Void> processBridgeEvent(final Message<String> message) {
+        try {
+            CloudEvent cloudEvent = decode(message.getPayload());
+            LOG.info("Received cloudevent with id {}", cloudEvent.getId());
+        } catch (RuntimeException e) {
+            LOG.error("Failed to handle Event received on Ansible gateway. The message is acked anyway.", e);
+        }
+
+        return message.ack();
+    }
+
+    private CloudEvent decode(String json) {
+        try {
+            return mapper.reader().readValue(json, CloudEvent.class);
+        } catch (IOException e) {
+            LOG.error("Unable to decode CloudEvent", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+}
