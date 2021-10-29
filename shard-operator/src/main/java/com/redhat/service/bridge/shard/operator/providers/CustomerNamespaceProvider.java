c550cedf24182aeb98d699388be7bb51f055b72c
Ricardo Zanini <1538000+ricardozanini@users.noreply.github.com>
2021-10-29 10:22:39 -0300

    [MGDOBR-92] - Introduce managed customer namespace (#94)
    * [MGDOBR-92] - Introduce managed customer namespace
    
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>
    
    * Rebase namespace handler with BridgeIngress deployment
    
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>
    
    * Updating README, improving CustomerNamespace handler
    
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>
    
    * Fix docs, making jib build the image by default
    
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>
    
    * Sanitize label values
    
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>
    
    * Fix formatting
    
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>
    
    * Wrapping log message variables with quotes
    
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>

--- shard-operator/src/main/java/com/redhat/service/bridge/shard/operator/providers/CustomerNamespaceProvider.java
+++ shard-operator/src/main/java/com/redhat/service/bridge/shard/operator/providers/CustomerNamespaceProvider.java
@@ -1,5 +1,43 @@
 package com.redhat.service.bridge.shard.operator.providers;
 
+import java.util.Locale;
+
+import io.fabric8.kubernetes.api.model.Namespace;
+import io.fabric8.kubernetes.client.utils.KubernetesResourceUtil;
+
+/**
+ * Service Interface for customer namespace handling.
+ */
 public interface CustomerNamespaceProvider {
-    String resolveNamespace(String customerId);
+
+    /**
+     * String format for the owned Bridge Namespace name.
+     */
+    String NS_PREFIX_FORMAT = "ob-%s";
+
+    /**
+     * Creates a new namespace with the given customer ID. If the namespace already exists, return it instead.
+     *
+     * @return the new namespace or the existing namespace based on the customer identification
+     */
+    Namespace fetchOrCreateCustomerNamespace(String customerId);
+
+    /**
+     * Deletes the given custom namespace if owned by the Shard Operator, and it does not have any OpenBridge CRs.
+     */
+    void deleteCustomerNamespaceIfEmpty(String customerId);
+
+    /**
+     * Get a sanitized name for the Customer Namespace with the given customer ID.
+     *
+     * @see <a href="https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names">Object Names and IDs</a>
+     */
+    default String resolveName(final String customerId) {
+        final String sanitizedName = KubernetesResourceUtil.sanitizeName(String.format(NS_PREFIX_FORMAT, customerId)).toLowerCase(Locale.ROOT);
+        if (KubernetesResourceUtil.isValidName(sanitizedName)) {
+            return sanitizedName;
+        }
+        // TODO: we have to either enforce a valid Kubernetes resource name for customer ids once it's inserted in our database or come up with an alternative way to create the namespace name.
+        throw new IllegalArgumentException(String.format("CustomerID '%s' can't be sanitized to a Kubernetes valid resource name", customerId));
+    }
 }
