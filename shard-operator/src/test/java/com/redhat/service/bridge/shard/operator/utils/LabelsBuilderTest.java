caec02810cb6365d35d12098de03d779c4f528f1
Jacopo Rota <jacopo.r00ta@gmail.com>
2021-11-03 18:20:40 +0100

    MGDOBR-103: refactoring

--- shard-operator/src/test/java/com/redhat/service/bridge/shard/operator/utils/LabelsBuilderTest.java
+++ shard-operator/src/test/java/com/redhat/service/bridge/shard/operator/utils/LabelsBuilderTest.java
@@ -15,6 +15,7 @@ public class LabelsBuilderTest {
     @Test
     public void buildWithDefaults() {
         assertThat(new LabelsBuilder().buildWithDefaults().size()).isEqualTo(2);
+        assertThat(new LabelsBuilder().buildWithDefaults()).containsKey(LabelsBuilder.MANAGED_BY_LABEL).containsKey(LabelsBuilder.CREATED_BY_LABEL);
         assertThat(new LabelsBuilder().withApplicationType("application").buildWithDefaults().size()).isEqualTo(3);
         assertThat(new LabelsBuilder().withCreatedBy("me").buildWithDefaults().size()).isEqualTo(2);
     }
