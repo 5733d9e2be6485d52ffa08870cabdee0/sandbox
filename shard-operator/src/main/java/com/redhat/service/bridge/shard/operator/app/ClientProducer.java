b6eeb16a3a152e063f9757af5965c8003e9026ca
Jacopo Rota <jacopo.r00ta@gmail.com>
2021-11-10 17:31:27 +0100

    MGDOBR-118: refactoring

--- shard-operator/src/main/java/com/redhat/service/bridge/shard/operator/app/ClientProducer.java
+++
@@ -1,7 +0,0 @@
-package com.redhat.service.bridge.shard.operator.app;
-
-import io.fabric8.openshift.client.OpenShiftClient;
-
-public interface ClientProducer {
-    OpenShiftClient produceClient();
-}
