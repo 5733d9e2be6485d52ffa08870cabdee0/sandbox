587c24c836a96fa44c7d0ea929c784721616e239
Jacopo Rota <jacopo.r00ta@gmail.com>
2021-11-12 07:42:37 +0100

    MGDOBR-118: shard operator configures Ingress application kafka topic (#110)
    * MGDOBR-118: shard operator configures ingress application

---
+++ shard-operator/src/main/java/com/redhat/service/bridge/shard/operator/providers/KafkaConfigurationCostants.java
@@ -0,0 +1,7 @@
+package com.redhat.service.bridge.shard.operator.providers;
+
+public class KafkaConfigurationCostants {
+    public static final String KAFKA_BOOTSTRAP_SERVERS_ENV_VAR = "KAFKA_BOOTSTRAP_SERVERS";
+    public static final String KAFKA_CLIENT_ID_ENV_VAR = "KAFKA_CLIENT_ID";
+    public static final String KAFKA_CLIENT_SECRET_ENV_VAR = "KAFKA_CLIENT_SECRET";
+}
