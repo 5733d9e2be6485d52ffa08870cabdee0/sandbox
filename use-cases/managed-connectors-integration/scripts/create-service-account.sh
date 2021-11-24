4c0e75b349707a77d24134d2c93e6cdafd123bf8
Luca Molteni <volothamp@gmail.com>
2021-11-24 17:12:36 +0100

    Fix service account creation

--- use-cases/managed-connectors-integration/scripts/create-service-account.sh
+++ use-cases/managed-connectors-integration/scripts/create-service-account.sh
@@ -9,5 +9,5 @@ OLD_SERVICE_ACCOUNT_ID=$(rhoas service-account list | grep $SERVICE_ACCOUNT_NAME
 
 rhoas service-account delete -y --id=$OLD_SERVICE_ACCOUNT_ID
 
-rhoas service-account create --output-file=./service-acct-credentials.json --file-format=json --overwrite --short-description=lucamolteni-managedconnector-serviceaccount
+rhoas service-account create --output-file=./service-acct-credentials.json --file-format=json --overwrite --short-description=$SERVICE_ACCOUNT_NAME
 
