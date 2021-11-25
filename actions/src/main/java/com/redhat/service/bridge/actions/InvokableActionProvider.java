ce6db9e7c0a5d0c2b8fca1d8ea285d8433832190
Alessandro Costa <alecosta.me@gmail.com>
2021-11-25 14:18:54 +0100

    MGDOBR-134: add missing InvokableActionProvider

---
+++ actions/src/main/java/com/redhat/service/bridge/actions/InvokableActionProvider.java
@@ -0,0 +1,9 @@
+package com.redhat.service.bridge.actions;
+
+import com.redhat.service.bridge.infra.models.actions.BaseAction;
+import com.redhat.service.bridge.infra.models.dto.ProcessorDTO;
+
+public interface InvokableActionProvider extends ActionProvider {
+
+    ActionInvoker getActionInvoker(ProcessorDTO processor, BaseAction baseAction);
+}
