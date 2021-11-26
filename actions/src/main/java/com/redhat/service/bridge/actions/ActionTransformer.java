fcb213355a84f69ca02437beeffddb183ef629e8
Alessandro Costa <alecosta.me@gmail.com>
2021-11-26 14:59:18 +0100

    MGDOBR-134: add comments to ActionTransformer and InvokableActionProvider

--- actions/src/main/java/com/redhat/service/bridge/actions/ActionTransformer.java
+++ actions/src/main/java/com/redhat/service/bridge/actions/ActionTransformer.java
@@ -4,6 +4,10 @@ import com.redhat.service.bridge.infra.models.actions.BaseAction;
 
 public interface ActionTransformer {
 
+    /**
+     * This is a helper transformer to be used whenever the action doesn't need
+     * to be modified by the transformer (e.g. for invokable actions).
+     */
     ActionTransformer IDENTITY = (action, bridgeId, customerId) -> action;
 
     BaseAction transform(BaseAction action, String bridgeId, String customerId);
--- actions/src/main/java/com/redhat/service/bridge/actions/InvokableActionProvider.java
+++ actions/src/main/java/com/redhat/service/bridge/actions/InvokableActionProvider.java
@@ -3,10 +3,17 @@ package com.redhat.service.bridge.actions;
 import com.redhat.service.bridge.infra.models.actions.BaseAction;
 import com.redhat.service.bridge.infra.models.dto.ProcessorDTO;
 
+/**
+ * Invokable actions can be directly invoked by the executor via their specific {@link ActionInvoker}.
+ */
 public interface InvokableActionProvider extends ActionProvider {
 
     ActionInvoker getActionInvoker(ProcessorDTO processor, BaseAction baseAction);
 
+    /**
+     * Usually invokable actions don't need to be transformed by the transformer.
+     * That's why the default implementation of this method returns the identity transformer.
+     */
     @Override
     default ActionTransformer getTransformer() {
         return ActionTransformer.IDENTITY;
