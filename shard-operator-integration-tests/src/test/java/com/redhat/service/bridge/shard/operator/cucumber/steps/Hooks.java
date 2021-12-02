522e0e870c1f402487daabae40ea5d54d10a60e3
Karel Suta <ksuta@redhat.com>
2021-12-02 10:51:17 +0100

    [MGDOBR-177] Store logs and events of Shard operator integration tests
    Signed-off-by: Karel Suta <ksuta@redhat.com>

--- shard-operator-integration-tests/src/test/java/com/redhat/service/bridge/shard/operator/cucumber/steps/Hooks.java
+++ shard-operator-integration-tests/src/test/java/com/redhat/service/bridge/shard/operator/cucumber/steps/Hooks.java
@@ -1,10 +1,14 @@
 package com.redhat.service.bridge.shard.operator.cucumber.steps;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import com.redhat.service.bridge.shard.operator.cucumber.common.Context;
+import com.redhat.service.bridge.shard.operator.cucumber.logs.EventCollector;
+import com.redhat.service.bridge.shard.operator.cucumber.logs.LogCollector;
 
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 
 /**
  * Cucumber hooks for setup and cleanup
@@ -17,8 +21,19 @@ public class Hooks {
         this.context = context;
     }
 
-    @After
-    public void cleanupNamespaceAfterScenario() throws FileNotFoundException {
+    @Before
+    public void before(Scenario scenario) {
+        this.context.setScenario(scenario);
+    }
+
+    @After(order = 10)
+    public void cleanupTestNamespaceAfterScenario() {
         context.getClient().namespaces().withName(context.getNamespace()).delete();
+    }
+
+    @After(order = 20)
+    public void storeTestNamespaceLogs() throws IOException {
+        LogCollector.storeNamespacePodLogs(context.getClient(), String.format("%s-%s", context.getScenario().getName(), context.getNamespace()), context.getNamespace());
+        EventCollector.storeNamespaceEvents(context.getClient(), String.format("%s-%s", context.getScenario().getName(), context.getNamespace()), context.getNamespace());
     }
 }
