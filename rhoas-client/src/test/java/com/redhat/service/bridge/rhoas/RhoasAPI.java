4674164de840e3f53336d46b2e077fb3b57189e7
Alessandro Costa <alecosta.me@gmail.com>
2022-01-05 10:24:14 +0100

    MGDOBR-168: add comments

--- rhoas-client/src/test/java/com/redhat/service/bridge/rhoas/RhoasAPI.java
+++ rhoas-client/src/test/java/com/redhat/service/bridge/rhoas/RhoasAPI.java
@@ -12,6 +12,12 @@ import javax.ws.rs.core.MediaType;
 import com.redhat.service.bridge.rhoas.dto.TopicAndServiceAccountRequest;
 import com.redhat.service.bridge.rhoas.dto.TopicAndServiceAccountResponse;
 
+/**
+ * This dummy test API simulates a real usage of the {@link RhoasClient} in a real JAX-RS bean.
+ * It allows {@link RhoasAPITest} to spot errors caused by the client mistakenly blocking the
+ * Vert.x event loop thread, which can't be spotted testing directly the client in a QuarkusTest
+ * (because blocking the test's main thread doesn't cause any error: it simply blocks).
+ */
 @Path("/rhoas/")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
