8975cca7dd799739153bb4861b56194a8df967a2
Jacopo Rota <jacopo.r00ta@gmail.com>
2022-01-11 14:18:56 +0100

    MGDOBR-209: refactoring

--- shard-operator/src/main/java/com/redhat/service/bridge/shard/operator/utils/Constants.java
+++ shard-operator/src/main/java/com/redhat/service/bridge/shard/operator/utils/Constants.java
@@ -3,5 +3,4 @@ package com.redhat.service.bridge.shard.operator.utils;
 public class Constants {
     public static final String BRIDGE_INGRESS_BRIDGE_ID_CONFIG_ENV_VAR = "EVENT_BRIDGE_BRIDGE_ID";
     public static final String BRIDGE_EXECUTOR_PROCESSOR_DEFINITION_ENV_VAR = "PROCESSOR_DEFINITION";
-    public static final int MAX_HTTP_RETRY = 5;
 }
