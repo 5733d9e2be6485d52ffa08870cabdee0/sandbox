af90d74f942f67ad01ce3166c3cbbd33790c8d65
Alessandro Costa <alecosta.me@gmail.com>
2022-01-12 17:05:25 +0100

    MGDOBR-222: produce app services clients

--- rhoas-client/src/main/java/com/redhat/service/bridge/rhoas/RhoasProducer.java
+++ rhoas-client/src/main/java/com/redhat/service/bridge/rhoas/RhoasProducer.java
@@ -4,7 +4,11 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
 
+import org.eclipse.microprofile.config.ConfigProvider;
+
 import io.quarkus.arc.properties.IfBuildProperty;
+import io.quarkus.oidc.client.OidcClients;
+import io.vertx.mutiny.core.Vertx;
 
 @Dependent
 public class RhoasProducer {
@@ -12,7 +16,24 @@ public class RhoasProducer {
     @Produces
     @ApplicationScoped
     @IfBuildProperty(name = RhoasClient.ENABLED_FLAG, stringValue = "true")
-    public RhoasClient produceRealRhoasClient(KafkasMgmtV1Client mgmtClient, KafkaInstanceAdminClient instanceClient) {
+    public RhoasClient produceRhoasClient(KafkasMgmtV1Client mgmtClient, KafkaInstanceAdminClient instanceClient) {
         return new RhoasClientImpl(mgmtClient, instanceClient);
+    }
+
+    @Produces
+    @ApplicationScoped
+    @IfBuildProperty(name = RhoasClient.ENABLED_FLAG, stringValue = "true")
+    public KafkaInstanceAdminClient produceKafkaInstanceAdminClientImpl(Vertx vertx, OidcClients oidcClients) {
+        String basePath = ConfigProvider.getConfig().getValue("event-bridge.rhoas.instance-api.host", String.class);
+        return new KafkaInstanceAdminClientImpl(vertx, basePath, oidcClients.getClient("mas-sso"));
+    }
+
+    @Produces
+    @ApplicationScoped
+    @IfBuildProperty(name = RhoasClient.ENABLED_FLAG, stringValue = "true")
+    public KafkasMgmtV1Client produceKafkasMgmtV1Client(Vertx vertx, OidcClients oidcClients) {
+        String basePath = ConfigProvider.getConfig().getValue("event-bridge.rhoas.mgmt-api.host", String.class);
+        String refreshToken = ConfigProvider.getConfig().getValue("event-bridge.rhoas.sso.red-hat.refresh-token", String.class);
+        return new KafkasMgmtV1ClientImpl(vertx, basePath, oidcClients.getClient("red-hat-sso"), refreshToken);
     }
 }
