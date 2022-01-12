d924de031da0f5c12a857bbd6bd4c4d9254685e4
Alessandro Costa <alecosta.me@gmail.com>
2022-01-12 17:29:40 +0100

    MGDOBR-222: add RhoasEnabledWithMissingPropsTestProfile

--- rhoas-client/src/test/java/com/redhat/service/bridge/rhoas/RhoasInjectionEnabledWithMissingPropsTest.java
+++ rhoas-client/src/test/java/com/redhat/service/bridge/rhoas/RhoasInjectionEnabledWithMissingPropsTest.java
@@ -6,17 +6,17 @@ import javax.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.junit.jupiter.api.Test;
 
-import com.redhat.service.bridge.test.rhoas.testprofiles.RhoasEnabledTestProfile;
+import com.redhat.service.bridge.test.rhoas.testprofiles.RhoasEnabledWithMissingPropsTestProfile;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @QuarkusTest
-@TestProfile(RhoasEnabledTestProfile.class)
-class RhoasInjectionEnabledTest {
+@TestProfile(RhoasEnabledWithMissingPropsTestProfile.class)
+class RhoasInjectionEnabledWithMissingPropsTest {
 
     @ConfigProperty(name = "event-bridge.feature-flags.rhoas-enabled")
     String enabled;
@@ -29,7 +29,7 @@ class RhoasInjectionEnabledTest {
         assertThat(enabled).isEqualTo("true");
         assertThat(rhoasClient.isUnsatisfied()).isFalse();
         assertThat(rhoasClient.isAmbiguous()).isFalse();
-        assertThatNoException().isThrownBy(() -> rhoasClient.get().toString());
+        assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> rhoasClient.get().toString());
     }
 
 }
