09635c60e1c09b522e76f6a09890a1840719d323
Jiri Petrlik <jiripetrlik@gmail.com>
2022-02-07 09:27:39 +0100

    MGDOBR-288 Remove oc command from startMinikubeDeployLocalDev script

--- kustomize/startMinikubeDeployLocalDev.sh
+++ kustomize/startMinikubeDeployLocalDev.sh
@@ -9,13 +9,13 @@ sed -i -E "s|(.*http://).*(:30007.*)|\1$(minikube ip)\2|" overlays/minikube/mana
 sleep 10s
 
 echo "Deploying all resources"
-kustomize build overlays/minikube | oc apply -f -
+kustomize build overlays/minikube | kubectl apply -f -
 
 status=$?
 if [ $status -ne 0 ]; then
   echo "Some resources fail to deploy (concurrency issues), redeploying"
   sleep 5s
-  kustomize build overlays/minikube | oc apply -f -
+  kustomize build overlays/minikube | kubectl apply -f -
 fi
 
 echo "Wait for Keycloak to start"
@@ -28,7 +28,7 @@ TOKEN=$(curl --insecure -X POST http://$MINIKUBE_IP:30007/auth/realms/event-brid
 sed -i -E "s|(.*WEBHOOK_TECHNICAL_BEARER_TOKEN=).*|\1$TOKEN|" overlays/minikube/shard/kustomization.yaml
 
 echo "Redeploy resources to apply token"
-kustomize build overlays/minikube | oc apply -f -
+kustomize build overlays/minikube | kubectl apply -f -
 kubectl delete pod --selector=app=event-bridge-shard-operator -n event-bridge-operator
 
 echo "Wait for manager and operator to start"
