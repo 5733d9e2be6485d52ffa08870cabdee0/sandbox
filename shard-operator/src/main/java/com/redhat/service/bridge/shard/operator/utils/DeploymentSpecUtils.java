ca94d573ce8ac243f35a29cac3624cb7727227d1
Alessandro Costa <alecosta.me@gmail.com>
2022-02-11 15:09:51 +0100

    MGDOBR-255 MGDOBR-256: update DeploymentSpecUtils

--- shard-operator/src/main/java/com/redhat/service/bridge/shard/operator/utils/DeploymentSpecUtils.java
+++ shard-operator/src/main/java/com/redhat/service/bridge/shard/operator/utils/DeploymentSpecUtils.java
@@ -8,6 +8,7 @@ public class DeploymentSpecUtils {
     public static boolean isDeploymentEqual(Deployment expected, Deployment existing) {
         return expected.getSpec().getSelector().getMatchLabels().equals(existing.getSpec().getSelector().getMatchLabels())
                 && expected.getSpec().getTemplate().getSpec().getContainers().get(0).getEnv().equals(existing.getSpec().getTemplate().getSpec().getContainers().get(0).getEnv())
-                && expected.getSpec().getTemplate().getSpec().getContainers().get(0).getImage().equals(existing.getSpec().getTemplate().getSpec().getContainers().get(0).getImage());
+                && expected.getSpec().getTemplate().getSpec().getContainers().get(0).getImage().equals(existing.getSpec().getTemplate().getSpec().getContainers().get(0).getImage())
+                && expected.getSpec().getProgressDeadlineSeconds().equals(existing.getSpec().getProgressDeadlineSeconds());
     }
 }
