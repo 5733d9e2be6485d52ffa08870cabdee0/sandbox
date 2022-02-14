c86a4feb83e8c14e73106b38b083d3985cd2e4ba
Jacopo Rota <jacopo.r00ta@gmail.com>
2022-02-14 09:43:42 +0100

    MGDOBR-293: refactoring manager database test utils

--- manager/src/test/java/com/redhat/service/bridge/manager/dao/ShardDAOTest.java
+++ manager/src/test/java/com/redhat/service/bridge/manager/dao/ShardDAOTest.java
@@ -26,7 +26,7 @@ public class ShardDAOTest {
 
     @BeforeEach
     public void before() {
-        databaseManagerUtils.cleanUpAndInitWithDefaultShard();
+        databaseManagerUtils.cleanUp();
     }
 
     @Test
