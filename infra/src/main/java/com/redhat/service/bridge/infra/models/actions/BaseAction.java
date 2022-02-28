8817e645e85a7479bd681698a7641f9e2cf9c81b
Michael Anstis <manstis@redhat.com>
2022-02-28 09:47:45 +0000

    Include @JsonIgnoreProperties(ignoreUnknown = true) as a good practice.

--- infra/src/main/java/com/redhat/service/bridge/infra/models/actions/BaseAction.java
+++ infra/src/main/java/com/redhat/service/bridge/infra/models/actions/BaseAction.java
@@ -7,9 +7,11 @@ import java.util.Objects;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class BaseAction {
 
