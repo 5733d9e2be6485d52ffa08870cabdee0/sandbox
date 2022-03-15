edfd7ce781b526d6807e26a8b73117497f4147d9
Luca Molteni <volothamp@gmail.com>
2022-03-15 17:04:53 +0100

    NO-JIRA Update minikube and k8s version (#441)
    * Update minikube and k8s version
    
    * Fix README

--- dev/bin/interaction/create-processor.sh
+++ dev/bin/interaction/create-processor.sh
@@ -23,8 +23,26 @@ export PROCESSOR_CREATION='{
   "transformationTemplate": "{\"test\": \"{data.myMessage}\"}"
 }'
 
+export PROCESSOR_CREATION_WEBHOOK='{
+   "name": '"\"$PROCESSOR_NAME\""',
+   "action": {
+      "type": "Webhook",
+      "parameters": {
+         "endpoint": '"\"$SLACK_WEBHOOK_URL\""'
+      }
+   },
+  "filters": [
+    {
+      "key": "source",
+      "type": "StringEquals",
+      "value": "StorageService"
+    }
+  ],
+  "transformationTemplate": "{\"text\": \"{data.myMessage}\"}"
+}'
+
 printf "\n\nCreating the processor with name $PROCESSOR_NAME\n"
-PROCESSOR_ID=$(curl -s -X POST -H "Authorization: $OB_TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' -d "$PROCESSOR_CREATION" $MANAGER_URL/api/v1/bridges/$BRIDGE_ID/processors | jq -r .id)
+PROCESSOR_ID=$(curl -s -X POST -H "Authorization: $OB_TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' -d "$PROCESSOR_CREATION_WEBHOOK" $MANAGER_URL/api/v1/bridges/$BRIDGE_ID/processors | jq -r .id)
 
 printf "\n\nProcessor Created: $PROCESSOR_NAME\n"
 echo "export PROCESSOR_ID=$PROCESSOR_ID"
