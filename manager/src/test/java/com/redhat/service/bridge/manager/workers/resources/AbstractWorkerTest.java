406f5be38378dccc820e580f8dfbe15cc9f1d8ac
Michael Anstis <manstis@redhat.com>
2022-03-22 13:08:28 +0000

    Updates following peer review.

--- manager/src/test/java/com/redhat/service/bridge/manager/workers/resources/AbstractWorkerTest.java
+++ manager/src/test/java/com/redhat/service/bridge/manager/workers/resources/AbstractWorkerTest.java
@@ -13,11 +13,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.redhat.service.bridge.infra.models.dto.ManagedResourceStatus;
 import com.redhat.service.bridge.manager.RhoasService;
-import com.redhat.service.bridge.manager.TestConstants;
 import com.redhat.service.bridge.manager.dao.BridgeDAO;
 import com.redhat.service.bridge.manager.models.Bridge;
 import com.redhat.service.bridge.manager.models.Work;
 import com.redhat.service.bridge.manager.utils.DatabaseManagerUtils;
+import com.redhat.service.bridge.manager.utils.Fixtures;
 import com.redhat.service.bridge.manager.workers.WorkManager;
 import com.redhat.service.bridge.test.resource.PostgresResource;
 
@@ -27,22 +27,19 @@ import io.quarkus.test.junit.mockito.InjectMock;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.verify;
 
 @QuarkusTest
 @ExtendWith(MockitoExtension.class)
 @QuarkusTestResource(PostgresResource.class)
 public class AbstractWorkerTest {
 
-    private static final String RESOURCE_ID = "123";
-
-    @InjectMock
-    WorkManager workManager;
-
     @InjectMock
     @SuppressWarnings("unused")
     //Needed to set up RHOAS for tests
     RhoasService rhoasService;
+
+    @Inject
+    WorkManager workManager;
 
     @Inject
     BridgeWorker worker;
@@ -67,59 +64,48 @@ public class AbstractWorkerTest {
     @Test
     @Transactional
     void workIsCompletedWhenResourceIsNotFound() {
-        Work work = new Work();
-        work.setType(Bridge.class.getName());
-        work.setSubmittedAt(ZonedDateTime.now());
-        work.setManagedResourceId(RESOURCE_ID);
+        Bridge bridge = Fixtures.createBridge();
+        // It is intentional that we do not persist the Bridge here
+        Work work = workManager.schedule(bridge);
+        assertThat(workManager.exists(work)).isTrue();
 
         assertThatThrownBy(() -> worker.handleWork(work)).isInstanceOf(IllegalStateException.class);
-
-        verify(workManager).complete(work);
     }
 
     @Test
     @Transactional
     void workIsCompletedWhenMaxRetriedExceeded() {
-        Bridge bridge = new Bridge(TestConstants.DEFAULT_BRIDGE_NAME);
-        bridge.setCustomerId(TestConstants.DEFAULT_CUSTOMER_ID);
-        bridge.setStatus(ManagedResourceStatus.ACCEPTED);
-        bridge.setSubmittedAt(ZonedDateTime.now());
-        bridge.setId(RESOURCE_ID);
+        Bridge bridge = Fixtures.createBridge();
+        // Persist Bridge so that it can be found by the Worker
         bridgeDAO.persist(bridge);
 
-        Work work = new Work();
-        work.setType(Bridge.class.getName());
-        work.setSubmittedAt(bridge.getSubmittedAt());
-        work.setManagedResourceId(RESOURCE_ID);
+        Work work = workManager.schedule(bridge);
+        assertThat(workManager.exists(work)).isTrue();
         work.setAttempts(maxRetries + 1);
 
         worker.handleWork(work);
 
         assertThat(bridge.getStatus()).isEqualTo(ManagedResourceStatus.FAILED);
 
-        verify(workManager).complete(work);
+        assertThat(workManager.exists(work)).isFalse();
     }
 
     @Test
     @Transactional
     void workIsCompletedWhenTimedOut() {
-        Bridge bridge = new Bridge(TestConstants.DEFAULT_BRIDGE_NAME);
-        bridge.setCustomerId(TestConstants.DEFAULT_CUSTOMER_ID);
-        bridge.setStatus(ManagedResourceStatus.ACCEPTED);
-        bridge.setSubmittedAt(ZonedDateTime.now().minusSeconds(timeoutSeconds * 2L));
-        bridge.setId(RESOURCE_ID);
+        Bridge bridge = Fixtures.createBridge();
+        // Persist Bridge so that it can be found by the Worker
         bridgeDAO.persist(bridge);
 
-        Work work = new Work();
-        work.setType(Bridge.class.getName());
-        work.setSubmittedAt(bridge.getSubmittedAt());
-        work.setManagedResourceId(RESOURCE_ID);
+        Work work = workManager.schedule(bridge);
+        assertThat(workManager.exists(work)).isTrue();
+        work.setSubmittedAt(ZonedDateTime.now().minusSeconds(timeoutSeconds * 2L));
 
         worker.handleWork(work);
 
         assertThat(bridge.getStatus()).isEqualTo(ManagedResourceStatus.FAILED);
 
-        verify(workManager).complete(work);
+        assertThat(workManager.exists(work)).isFalse();
     }
 
 }
