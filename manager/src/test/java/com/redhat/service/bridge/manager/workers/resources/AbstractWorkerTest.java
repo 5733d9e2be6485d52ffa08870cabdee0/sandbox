f3a59d6c1db978987806c467def04c20f0a2c772
Michael Anstis <manstis@users.noreply.github.com>
2022-03-22 08:30:24 +0000

    MGDOBR-454: Work is never removed from the queue if the entity has been deleted (#475)
    * MGDOBR-454: Work is never removed from the queue if the entity has been deleted
    
    * Fix dirty ready problem.
    
    * Improve robustness of tests.
    
    * Simplify test
    
    * Refactor duplicated code.
    
    Co-authored-by: Michael Anstis <manstis@redhat.com>

---
+++ manager/src/test/java/com/redhat/service/bridge/manager/workers/resources/AbstractWorkerTest.java
@@ -0,0 +1,56 @@
+package com.redhat.service.bridge.manager.workers.resources;
+
+import java.time.ZonedDateTime;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.redhat.service.bridge.manager.RhoasService;
+import com.redhat.service.bridge.manager.models.Bridge;
+import com.redhat.service.bridge.manager.models.Work;
+import com.redhat.service.bridge.manager.workers.WorkManager;
+import com.redhat.service.bridge.test.resource.PostgresResource;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.verify;
+
+@QuarkusTest
+@ExtendWith(MockitoExtension.class)
+@QuarkusTestResource(PostgresResource.class)
+public class AbstractWorkerTest {
+
+    private static final String RESOURCE_ID = "123";
+
+    @InjectMock
+    WorkManager workManager;
+
+    @InjectMock
+    @SuppressWarnings("unused")
+    //Needed to set up RHOAS for tests
+    RhoasService rhoasService;
+
+    @Inject
+    BridgeWorker worker;
+
+    @Test
+    @Transactional
+    void workIsCompletedWhenResourceIsNotFound() {
+        Work work = new Work();
+        work.setType(Bridge.class.getName());
+        work.setSubmittedAt(ZonedDateTime.now());
+        work.setManagedResourceId(RESOURCE_ID);
+
+        assertThatThrownBy(() -> worker.handleWork(work)).isInstanceOf(IllegalStateException.class);
+
+        verify(workManager).complete(work);
+    }
+
+}
