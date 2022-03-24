9ca5832c24868f13b90420124ba4aaad63515b7d
Mauricio Magnani <msmagnanijr@gmail.com>
2022-03-24 13:31:24 -0300

    [NO-JIRA] - fix typo and update script (#520)
    Signed-off-by: Mauricio Magnani <msmagnanijr@gmail.com>

--- app-interface/app_sre_fleet_manager_build_push.sh
+++ app-interface/app_sre_fleet_manager_build_push.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-set -exv
-
-IMAGE_TAG=`git rev-parse --short=7 HEAD`
 IMAGE_NAME="quay.io/app-sre/rhose-fleet-manager"
+IMAGE_TAG=$(git rev-parse --short=7 HEAD)
 
-if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
-    echo "QUAY_USER and QUAY_TOKEN must be set"
-    exit 1
-fi
+docker build -f ../docker/Dockerfile.jvm -t "${IMAGE_NAME}:latest" ../manager
+docker tag "${IMAGE_NAME}:latest" "${IMAGE_NAME}:${IMAGE_TAG}"
 
-DOCKER_CONF="$PWD/.docker"
-mkdir -p "$DOCKER_CONF"
-docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
-docker --config="$DOCKER_CONF" build -f ../docker/Dockerfile.jvm -t "${IMAGE_NAME}:latest" ../manager
-docker --config="$DOCKER_CONF" tag "${IMAGE_NAME}:latest" "${IMAGE_NAME}:${IMAGE_TAG}"
-docker --config="$DOCKER_CONF" push "${IMAGE_NAME}:latest"
-docker --config="$DOCKER_CONF" push "${IMAGE_NAME}:${IMAGE_TAG}"
+DOCKER_CONF="${PWD}/.docker"
+mkdir -p "${DOCKER_CONF}"
+docker --config="${DOCKER_CONF}" login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
 
+docker --config="${DOCKER_CONF}" push "${IMAGE_NAME}:latest"
+docker --config="${DOCKER_CONF}" push "${IMAGE_NAME}:${IMAGE_TAG}"
