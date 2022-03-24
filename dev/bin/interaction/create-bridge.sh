0e80c35f57f4fa3a6d5a0e2eac2d7c337ea77d7f
Luca Molteni <volothamp@gmail.com>
2022-03-24 10:14:05 +0100

    NO-JIRA Update interaction scripts (#511)
    * Renamed script

--- dev/bin/interaction/create-bridge.sh
+++ dev/bin/interaction/create-bridge.sh
@@ -8,7 +8,7 @@ BRIDGE_NAME=${1:-$TODAY_BRIDGE_NAME}
 printf "\n\nCreating the bridge with name $BRIDGE_NAME"
 NEW_BRIDGE_ID=$(curl -s -X POST -H "Authorization: $OB_TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' -d "{\"name\": \"$BRIDGE_NAME\"}" $MANAGER_URL/api/v1/bridges | jq -r .id)
 
-printf "Bridge $BRIDGE_NAME created, set the bridge id\n\n"
-echo "export BRIDGE_ID=$NEW_BRIDGE_ID"
+printf "\nBridge $BRIDGE_NAME created, set the bridge id\n\n"
+echo "\texport BRIDGE_ID=$NEW_BRIDGE_ID"
 printf "\n"
 
