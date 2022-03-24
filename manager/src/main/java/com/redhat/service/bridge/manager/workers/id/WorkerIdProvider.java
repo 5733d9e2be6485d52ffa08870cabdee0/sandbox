bd1dbc51acde16578bcb84476acb261c9a122ad3
Luca Molteni <volothamp@gmail.com>
2022-03-24 16:46:38 +0100

    Use stable workerId

--- manager/src/main/java/com/redhat/service/bridge/manager/workers/id/WorkerIdProvider.java
+++ manager/src/main/java/com/redhat/service/bridge/manager/workers/id/WorkerIdProvider.java
@@ -4,13 +4,33 @@ import java.util.UUID;
 
 import javax.enterprise.inject.Produces;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class WorkerIdProvider {
 
-    private static final String WORKER_ID = UUID.randomUUID().toString();
+    private static final String RANDOM_STATIC_WORKER_ID = UUID.randomUUID().toString();
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WorkerIdProvider.class);
+
+    private String findPodNameFromKubernetes() {
+        String hostname = System.getenv("HOSTNAME");
+        LOGGER.trace("POD id from hostname: {}", hostname);
+
+        if (hostname != null && !hostname.isEmpty() && !"localhost".equals(hostname)) {
+            return "worker-" + hostname;
+        } else {
+            return null;
+        }
+    }
 
     @Produces
     public String getWorkerId() {
-        return WORKER_ID;
+        String podNameFromKubernetes = findPodNameFromKubernetes();
+        if(podNameFromKubernetes == null) {
+            return RANDOM_STATIC_WORKER_ID;
+        } else {
+            return podNameFromKubernetes;
+        }
     }
-
 }
