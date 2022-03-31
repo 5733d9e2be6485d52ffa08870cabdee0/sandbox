145f890dd86aa3df9e55ea8fd3b312b0bafbdce7
Jacopo Rota <jacopo.r00ta@gmail.com>
2022-03-31 22:42:23 +0200

    MGDOBR-483: move dev env to sso.redhat.com (#558)
    * MGDOBR-483: move dev env to sso.redhat.com

--- dev/bin/interaction/get-token.sh
+++ dev/bin/interaction/get-token.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # This needs to be called with source get-token.sh
 
-export OB_TOKEN="Bearer $(curl -s --insecure -X POST $KEYCLOAK_URL/auth/realms/event-bridge-fm/protocol/openid-connect/token --user event-bridge:secret -H 'content-type: application/x-www-form-urlencoded' -d "username=$KEYCLOAK_USERNAME&password=$KEYCLOAK_PASSWORD&grant_type=password" | jq --raw-output '.access_token')"
-
+if [[ "$TARGET_ENVIRONMENT" == "local" ]]; then
+  export OB_TOKEN="Bearer $(curl -s --insecure -X POST $KEYCLOAK_URL/auth/realms/event-bridge-fm/protocol/openid-connect/token --user event-bridge:secret -H 'content-type: application/x-www-form-urlencoded' -d "username=$KEYCLOAK_USERNAME&password=$KEYCLOAK_PASSWORD&grant_type=password" | jq --raw-output '.access_token')"
+elif [[ "$TARGET_ENVIRONMENT" == "remote" ]]; then
+  export OB_TOKEN="Bearer $(curl -s --insecure -X POST $SSO_REDHAT_URL/auth/realms/redhat-external/protocol/openid-connect/token --header 'Content-Type: application/x-www-form-urlencoded' --data-urlencode 'client_id=cloud-services' --data-urlencode 'grant_type=refresh_token' --data-urlencode "refresh_token=$OPENSHIFT_OFFLINE_TOKEN" | jq --raw-output '.access_token')"
+else
+  echo "Unknown value for TARGET_ENVIRONMENT. Valid values are [local, remote]"
+  return 1
+fi
 echo '- Token Set'
