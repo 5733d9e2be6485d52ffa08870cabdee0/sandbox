0aea2c517f4505170ad25e05854fc88ddf9ca14e
Alessandro Costa <alecosta.me@gmail.com>
2022-04-21 15:28:54 +0200

    MGDOBR-322: use Objects.equals in GatewayBean accept

--- processor/core/src/main/java/com/redhat/service/smartevents/processor/GatewayBean.java
+++ processor/core/src/main/java/com/redhat/service/smartevents/processor/GatewayBean.java
@@ -1,9 +1,11 @@
 package com.redhat.service.smartevents.processor;
 
+import java.util.Objects;
+
 public interface GatewayBean {
     String getType();
 
     default boolean accept(String gatewayType) {
-        return getType().equals(gatewayType);
+        return Objects.equals(getType(), gatewayType);
     }
 }
