55e6ec2de80a53e6cbe690041952bc754197abcb
Alessandro Costa <alecosta.me@gmail.com>
2022-04-26 16:58:56 +0200

    MGDOBR-322: improve migration script

--- manager/src/main/resources/db/migration/0.1/V0.1.9__update_processor.sql
+++ manager/src/main/resources/db/migration/0.1/V0.1.9__update_processor.sql
@@ -1,2 +1,11 @@
+-- add new "type" column
 alter table PROCESSOR
-    add column "type" varchar(255) default 'SINK';
+    add column "type" varchar(255);
+
+-- update existing processors "type" with 'SINK' value (the only possible one before this migration)
+update PROCESSOR
+    set "type"='SINK';
+
+-- add not null constraint to "type" column
+alter table PROCESSOR
+    alter column "type" set not null;
