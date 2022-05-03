4fd091565b7532cf34834e065e2acaa752abc5ca
Jacopo Rota <jacopo.r00ta@gmail.com>
2022-05-03 16:03:25 +0200

    Update rebase-and-deploy-to-env.sh

--- .github/scripts/rebase-and-deploy-to-env.sh
+++ .github/scripts/rebase-and-deploy-to-env.sh
@@ -35,7 +35,7 @@ if [ $(git branch --contains $SHA_COMMIT | grep -c "$TARGET_BRANCH") -ne 0 ]; th
 else
   git checkout -b $TARGET_BRANCH > /dev/null 2>&1
   git rebase $SHA_COMMIT > /dev/null 2>&1
-  git push origin $TARGET_BRANCH &> /dev/null
+  git push origin $TARGET_BRANCH > /dev/null 2>&1
   printf "\U2705 $SHA_COMMIT was not on $TARGET_BRANCH branch. $TARGET_BRANCH branch has been rebased and pushed to the upstream repository. The deployment is on the way!"
 fi
 
