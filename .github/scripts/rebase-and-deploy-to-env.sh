c0ea6796bbd9102d3f484faa1260c01008a92aaa
Jacopo Rota <jacopo.r00ta@gmail.com>
2022-05-03 10:38:36 +0200

    Update rebase-and-deploy-to-env.sh

--- .github/scripts/rebase-and-deploy-to-env.sh
+++ .github/scripts/rebase-and-deploy-to-env.sh
@@ -14,6 +14,11 @@ fi
 
 UPSTREAM_REPO_LOCATION=/tmp/upstream
 git clone https://$AUTHOR:$GITHUB_TOKEN@github.com/5733d9e2be6485d52ffa08870cabdee0/sandbox.git $UPSTREAM_REPO_LOCATION > /dev/null 2>&1
+# peek branches 
+git checkout --track origin/dev
+git checkout --track origin/stable
+git checkout main
+
 cd $UPSTREAM_REPO_LOCATION
 
 # If the deployment targets `stable`, then the feature must be on dev first.
