bb381dea463977b4d5ddb661be688a543e973c08
Alessandro Costa <alecosta.me@gmail.com>
2022-05-11 09:51:00 +0200

    MGDOBR-714: Add support for basic auth to Webhook action

--- processor/configuration/src/main/java/com/redhat/service/smartevents/processor/actions/webhook/WebhookActionValidator.java
+++ processor/configuration/src/main/java/com/redhat/service/smartevents/processor/actions/webhook/WebhookActionValidator.java
@@ -14,6 +14,8 @@ public class WebhookActionValidator implements WebhookAction, GatewayValidator<A
 
     public static final String MISSING_ENDPOINT_PARAM_MESSAGE = "Missing or empty \"endpoint\" parameter";
     public static final String MALFORMED_ENDPOINT_PARAM_MESSAGE = "Malformed \"endpoint\" URL";
+    public static final String BASIC_AUTH_CONFIGURATION_MESSAGE = "Basic authentication configuration error. " +
+            "\"" + BASIC_AUTH_USERNAME_PARAM + "\" and \"" + BASIC_AUTH_PASSWORD_PARAM + "\" must be both present and non empty.";
     public static final String INVALID_PROTOCOL_MESSAGE = "The \"endpoint\" protocol must be either \"http\" or \"https\"";
     public static final String RESERVED_ATTRIBUTES_USAGE_MESSAGE = "Some reserved parameters have been added to the request.";
 
@@ -33,6 +35,16 @@ public class WebhookActionValidator implements WebhookAction, GatewayValidator<A
 
         if (action.getParameters().containsKey(USE_TECHNICAL_BEARER_TOKEN_PARAM)) {
             return ValidationResult.invalid(RESERVED_ATTRIBUTES_USAGE_MESSAGE);
+        }
+
+        if (action.getParameters().containsKey(BASIC_AUTH_USERNAME_PARAM) && !action.getParameters().containsKey(BASIC_AUTH_PASSWORD_PARAM)
+                || !action.getParameters().containsKey(BASIC_AUTH_USERNAME_PARAM) && action.getParameters().containsKey(BASIC_AUTH_PASSWORD_PARAM)) {
+            return ValidationResult.invalid(BASIC_AUTH_CONFIGURATION_MESSAGE);
+        }
+
+        if (action.getParameters().containsKey(BASIC_AUTH_USERNAME_PARAM) && action.getParameters().containsKey(BASIC_AUTH_PASSWORD_PARAM)
+                && (action.getParameters().get(BASIC_AUTH_USERNAME_PARAM).isEmpty() || action.getParameters().get(BASIC_AUTH_PASSWORD_PARAM).isEmpty())) {
+            return ValidationResult.invalid(BASIC_AUTH_CONFIGURATION_MESSAGE);
         }
 
         URL endpointUrl;
