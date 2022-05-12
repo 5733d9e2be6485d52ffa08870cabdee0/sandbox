72d371c2366843954867c8c1e01b7d5dd70d4828
Vaibhav Jain <vajain@redhat.com>
2022-05-12 17:13:36 +0530

    removing default initialization values

--- manager/src/main/resources/db/migration/0.1/V0.1.11__update_bridge.sql
+++ manager/src/main/resources/db/migration/0.1/V0.1.11__update_bridge.sql
@@ -1,11 +1,3 @@
 -- add new "organisation_id" column
 alter table BRIDGE
-    add column "organisation_id" varchar(255);
-
--- update existing BRIDGE "organisation_id" with '15247674' value (the only possible one before this migration)
-update BRIDGE
-set "organisation_id"='15247674';
-
--- add not null constraint to "organisation_id" column
-alter table BRIDGE
-    alter column "organisation_id" set not null;
+    add column "organisation_id" varchar(255) NOT NULL;
