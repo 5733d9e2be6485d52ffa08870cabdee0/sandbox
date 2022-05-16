8c4f4966627591e755304a6fec7219cc4cb92d96
Vaibhav Jain <vajain@redhat.com>
2022-05-16 10:30:11 +0530

    Fix sonar cloud issues

--- infra/src/test/java/com/redhat/service/smartevents/infra/IdentityResolverTest.java
+++ infra/src/test/java/com/redhat/service/smartevents/infra/IdentityResolverTest.java
@@ -56,25 +56,25 @@ public class IdentityResolverTest {
     @Test
     public void testOrganisationIdResolver_resolveWithUserToken() {
         JsonWebToken jwt = mock(JsonWebToken.class);
-        when(jwt.getClaim(eq(APIConstants.ORG_ID_USER_ATTRIBUTE_CLAIM))).thenReturn(ORGANISATION_ID);
-        when(jwt.containsClaim(eq(APIConstants.ORG_ID_USER_ATTRIBUTE_CLAIM))).thenReturn(true);
+        when(jwt.getClaim(APIConstants.ORG_ID_USER_ATTRIBUTE_CLAIM)).thenReturn(ORGANISATION_ID);
+        when(jwt.containsClaim(APIConstants.ORG_ID_USER_ATTRIBUTE_CLAIM)).thenReturn(true);
         assertThat(identityResolver.resolveOrganisationId(jwt)).isEqualTo(ORGANISATION_ID);
     }
 
     @Test
     public void testOrganisationIdResolver_resolveWithServiceAccountToken() {
         JsonWebToken jwt = mock(JsonWebToken.class);
-        when(jwt.containsClaim(eq(APIConstants.ORG_ID_USER_ATTRIBUTE_CLAIM))).thenReturn(false);
-        when(jwt.containsClaim(eq(APIConstants.ORG_ID_SERVICE_ACCOUNT_ATTRIBUTE_CLAIM))).thenReturn(true);
-        when(jwt.getClaim(eq(APIConstants.ORG_ID_SERVICE_ACCOUNT_ATTRIBUTE_CLAIM))).thenReturn(ORGANISATION_ID);
+        when(jwt.containsClaim(APIConstants.ORG_ID_USER_ATTRIBUTE_CLAIM)).thenReturn(false);
+        when(jwt.containsClaim(APIConstants.ORG_ID_SERVICE_ACCOUNT_ATTRIBUTE_CLAIM)).thenReturn(true);
+        when(jwt.getClaim(APIConstants.ORG_ID_SERVICE_ACCOUNT_ATTRIBUTE_CLAIM)).thenReturn(ORGANISATION_ID);
         assertThat(identityResolver.resolveOrganisationId(jwt)).isEqualTo(ORGANISATION_ID);
     }
 
     @Test
     public void testValidTokenWithoutOrganisationIdClaims() {
         JsonWebToken jwt = mock(JsonWebToken.class);
-        when(jwt.containsClaim(eq(APIConstants.ORG_ID_SERVICE_ACCOUNT_ATTRIBUTE_CLAIM))).thenReturn(false);
-        when(jwt.containsClaim(eq(APIConstants.ORG_ID_USER_ATTRIBUTE_CLAIM))).thenReturn(false);
+        when(jwt.containsClaim(APIConstants.ORG_ID_SERVICE_ACCOUNT_ATTRIBUTE_CLAIM)).thenReturn(false);
+        when(jwt.containsClaim(APIConstants.ORG_ID_USER_ATTRIBUTE_CLAIM)).thenReturn(false);
         assertThatThrownBy(() -> identityResolver.resolve(jwt)).isInstanceOf(ForbiddenRequestException.class);
     }
 }
