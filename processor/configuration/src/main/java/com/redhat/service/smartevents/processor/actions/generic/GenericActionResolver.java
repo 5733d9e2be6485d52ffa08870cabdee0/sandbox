2fbeecbb2ae5da3a4901ca64060cca8d83a62edd
Luca Molteni <volothamp@gmail.com>
2022-05-19 21:31:01 +0200

    Action resolver

---
+++ processor/configuration/src/main/java/com/redhat/service/smartevents/processor/actions/generic/GenericActionResolver.java
@@ -0,0 +1,38 @@
+package com.redhat.service.smartevents.processor.actions.generic;
+
+import java.util.Map;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import com.redhat.service.smartevents.infra.models.gateways.Action;
+import com.redhat.service.smartevents.processor.GatewayConfiguratorService;
+import com.redhat.service.smartevents.processor.GatewayResolver;
+import com.redhat.service.smartevents.processor.actions.kafkatopic.KafkaTopicAction;
+
+@ApplicationScoped
+public class GenericActionResolver implements GatewayResolver<Action> {
+
+    @Inject
+    GatewayConfiguratorService gatewayConfiguratorService;
+
+    @Override
+    public Action resolve(Action action, String customerId, String bridgeId, String processorId) {
+
+        Action resolvedAction = new Action();
+
+        Map<String, String> newParameters = resolvedAction.getParameters();
+        newParameters.putAll(action.getParameters());
+
+        resolvedAction.setType(KafkaTopicAction.TYPE);
+
+        newParameters.put(KafkaTopicAction.TOPIC_PARAM, gatewayConfiguratorService.getConnectorTopicName(processorId));
+
+        return resolvedAction;
+    }
+
+    @Override
+    public String getType() {
+        return "Generic";
+    }
+}
