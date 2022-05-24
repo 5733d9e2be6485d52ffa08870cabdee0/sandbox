3738c8bc9251e3f16ad1975c7fb52a4b7b9210e5
Jacopo Rota <jacopo.r00ta@gmail.com>
2022-05-24 08:32:06 +0200

    fix migrations

--- manager/src/main/resources/db/migration/0.1/V0.1.12__update_bridge.sql
+++ manager/src/main/resources/db/migration/0.1/V0.1.12__update_bridge.sql
@@ -1,3 +1,4 @@
 -- add new "organisation_id" column
-alter table BRIDGE
-    add column "organisation_id" varchar(255) NOT NULL;
+alter table BRIDGE add column organisation_id varchar(255);
+update BRIDGE set organisation_id='';
+alter table BRIDGE alter column organisation_id set NOT NULL;
--- manager/src/main/resources/db/migration/0.1/V0.1.13__add_owner.sql
+++ manager/src/main/resources/db/migration/0.1/V0.1.13__add_owner.sql
@@ -1,6 +1,8 @@
 -- add new "owner" column
-alter table BRIDGE
-    add column "owner" varchar(255) NOT NULL;
+alter table BRIDGE add column owner varchar(255);
+update BRIDGE set owner='';
+alter table BRIDGE alter column owner set NOT NULL;
 
-alter table PROCESSOR
-    add column "owner" varchar(255) NOT NULL;
+alter table PROCESSOR add column owner varchar(255);
+update PROCESSOR set owner='';
+alter table PROCESSOR alter column owner set NOT NULL;
