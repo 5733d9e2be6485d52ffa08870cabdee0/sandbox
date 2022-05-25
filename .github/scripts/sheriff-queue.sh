e5519b9ac82a7932a7a383e5bace55982ab3e3a0
Savitha Thundathil Jose <sthundat@localhost.localdomain>
2022-05-25 11:04:54 +0530

    Implement review comments

--- .github/scripts/sheriff-queue.sh
+++ .github/scripts/sheriff-queue.sh
@@ -1,8 +1,7 @@
-date_string=$(sed '1!d' ./.github/data/sheriff-order)
+date_string="220509"
 start_date=$(date --date=$date_string)
 count=1
 total_records=$(wc -l <./.github/data/sheriff-order)
-let "total_records--"
 while [ $(date --date="$start_date" +%s) -lt $(date +%s) ]; do
   DOW=$(date -d "$start_date" +%u)
   if [ \( $DOW -eq 4 \) -o \( $DOW -eq 5 \) ]; then
@@ -14,7 +13,7 @@ while [ $(date --date="$start_date" +%s) -lt $(date +%s) ]; do
     count=1
   fi
   let "count++"
-
 done
 
+let "count--"
 head -$count ./.github/data/sheriff-order | tail -1
