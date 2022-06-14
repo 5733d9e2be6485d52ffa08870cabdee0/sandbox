5ea9af4bb101929b2a9c4aeb8e2ca7bee210f9f3
Luca Molteni <volothamp@gmail.com>
2022-06-14 14:20:42 +0200

    Fix sed used in istio script to work on BSD (macOS)  (#827)
    * Fix sed to work on BSD (macOS) see https://stackoverflow.com/a/24717687/4206
    * always install istio
    * avoid modifying commited file

--- dev/bin/minikube-start.sh
+++ dev/bin/minikube-start.sh
@@ -48,11 +48,11 @@ if [ "${disable_extra_components}" != 'true' ]; then
   sleep 5
   kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/kube-prometheus/v0.9.0/manifests/setup/prometheus-operator-0servicemonitorCustomResourceDefinition.yaml
   . "${SCRIPT_DIR_PATH}/knative-installer.sh"
-  istioctl manifest apply --set profile=default --set values.gateways.istio-ingressgateway.type="ClusterIP"
-  sed -i -E "s|(.*http://).*(:30007.*)|\1$(minikube -p "${MINIKUBE_PROFILE}" ip)\2|" ${KUSTOMIZE_DIR}/overlays/minikube/istio/jwt-request-authentication.yaml
+  yes | istioctl manifest apply --set profile=default --set values.gateways.istio-ingressgateway.type="ClusterIP"
   kubectl apply -f ${KUSTOMIZE_DIR}/overlays/minikube/istio/gateway.yaml
   kubectl apply -f ${KUSTOMIZE_DIR}/overlays/minikube/istio/virtual-service-kafka-broker.yaml
   kubectl apply -f ${KUSTOMIZE_DIR}/overlays/minikube/istio/jwt-request-authentication.yaml
+  cat ${KUSTOMIZE_DIR}/overlays/minikube/istio/jwt-request-authentication.yaml | sed -E "s|<REPLACE_WITH_MINIKUBE_IP>|$(minikube -p "${MINIKUBE_PROFILE}" ip)|" | kubectl apply -f -
 fi
 
 set +x
