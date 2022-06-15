d3cdff0ab9f8fb7201f146851ee3a77f233c998a
Jozef Marko <jomarko@redhat.com>
2022-06-15 17:00:01 +0200

    Read slack messages history no older than one day
    For more details see:
    - https://api.slack.com/methods/conversations.history

--- integration-tests/src/test/java/com/redhat/service/smartevents/integration/tests/resources/SlackResource.java
+++ integration-tests/src/test/java/com/redhat/service/smartevents/integration/tests/resources/SlackResource.java
@@ -1,5 +1,8 @@
 package com.redhat.service.smartevents.integration.tests.resources;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import com.redhat.service.smartevents.integration.tests.common.Utils;
@@ -10,18 +13,21 @@ import static io.restassured.RestAssured.given;
 
 public class SlackResource {
 
-    private final static String SLACK_URI = "https://slack.com/api/conversations.history?channel={channel}";
+    private final static String SLACK_URI = "https://slack.com/api/conversations.history";
     private final static String SLACK_CHANNEL = Utils.getSystemProperty("slack.channel");
     private final static String SLACK_TOKEN = Utils.getSystemProperty("slack.webhook.token");
     private final static String SLACK_WEBHOOK = Utils.getSystemProperty("slack.webhook.url");
 
     public static List<String> getListOfSlackMessages() {
+        final LocalDate yesterday = LocalDate.now(ZoneId.systemDefault()).minusDays(1);
         return given()
                 .auth()
                 .oauth2(SLACK_TOKEN)
                 .contentType(ContentType.JSON)
+                .queryParam("channel", SLACK_CHANNEL)
+                .queryParam("latest", yesterday.toEpochDay())
                 .when()
-                .get(SLACK_URI, SLACK_CHANNEL)
+                .get(SLACK_URI)
                 .then()
                 .log().ifValidationFails()
                 .statusCode(200)
