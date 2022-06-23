48131d0c86659dd25822f7cfd87483ea4ab30b2a
Jozef Marko <jomarko@redhat.com>
2022-06-23 09:44:57 +0200

    Fix BaseResponse import order

--- infra/src/main/java/com/redhat/service/smartevents/infra/api/models/responses/BaseResponse.java
+++ infra/src/main/java/com/redhat/service/smartevents/infra/api/models/responses/BaseResponse.java
@@ -1,8 +1,9 @@
 package com.redhat.service.smartevents.infra.api.models.responses;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Schema(name = "ObjectReference")
