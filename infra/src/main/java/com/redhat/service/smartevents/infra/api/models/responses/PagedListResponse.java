d1eb94ca67fc2b447cb7da826e166da3329e41a4
Jozef Marko <jomarko@redhat.com>
2022-06-23 07:44:23 +0200

    Fix PagedListResponse import order

--- infra/src/main/java/com/redhat/service/smartevents/infra/api/models/responses/PagedListResponse.java
+++ infra/src/main/java/com/redhat/service/smartevents/infra/api/models/responses/PagedListResponse.java
@@ -5,10 +5,11 @@ import java.util.stream.Collectors;
 
 import javax.validation.constraints.NotNull;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.redhat.service.smartevents.infra.models.ListResult;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Schema(name = "List",
