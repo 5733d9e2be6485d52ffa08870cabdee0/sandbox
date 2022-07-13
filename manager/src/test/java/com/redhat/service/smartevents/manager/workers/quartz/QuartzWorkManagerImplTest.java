71ce13d33688c62376e77ace219720aa88aa8ec4
Michael Anstis <manstis@redhat.com>
2022-07-13 14:11:51 +0100

    More Unit Tests..

--- manager/src/test/java/com/redhat/service/smartevents/manager/workers/quartz/QuartzWorkManagerImplTest.java
+++ manager/src/test/java/com/redhat/service/smartevents/manager/workers/quartz/QuartzWorkManagerImplTest.java
@@ -9,9 +9,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.quartz.JobDataMap;
 import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
 import org.quartz.Trigger;
+import org.quartz.TriggerKey;
 
+import com.redhat.service.smartevents.manager.models.Bridge;
 import com.redhat.service.smartevents.manager.models.Processor;
+import com.redhat.service.smartevents.manager.utils.Fixtures;
 import com.redhat.service.smartevents.manager.workers.Work;
 
 import static com.redhat.service.smartevents.manager.workers.quartz.QuartzWorkManagerImpl.STATE_FIELD_ATTEMPTS;
@@ -35,6 +39,9 @@ public class QuartzWorkManagerImplTest {
     @Captor
     ArgumentCaptor<Trigger> triggerArgumentCaptor;
 
+    @Captor
+    ArgumentCaptor<TriggerKey> triggerKeyArgumentCaptor;
+
     private QuartzWorkManagerImpl manager;
 
     @BeforeEach
@@ -42,12 +49,11 @@ public class QuartzWorkManagerImplTest {
         this.manager = new QuartzWorkManagerImpl();
         this.manager.quartz = quartz;
         this.manager.init();
-
-        when(resource.getId()).thenReturn(RESOURCE_ID);
     }
 
     @Test
-    void scheduleDoesNotRescheduleForExistingWork() throws Exception {
+    void testSchedule() throws Exception {
+        when(resource.getId()).thenReturn(RESOURCE_ID);
         Work work = manager.schedule(resource);
 
         verify(quartz).scheduleJob(triggerArgumentCaptor.capture());
@@ -60,6 +66,62 @@ public class QuartzWorkManagerImplTest {
         JobDataMap data = trigger.getJobDataMap();
         assertThat(data.getString(STATE_FIELD_ID)).isEqualTo(RESOURCE_ID);
         assertThat(data.getLong(STATE_FIELD_ATTEMPTS)).isZero();
+        assertThat(data.get(STATE_FIELD_SUBMITTED_AT)).isNotNull();
+    }
+
+    @Test
+    void testReschedule() throws Exception {
+        when(resource.getId()).thenReturn(RESOURCE_ID);
+        Work work = manager.schedule(resource);
+
+        manager.reschedule(work);
+
+        verify(quartz).rescheduleJob(triggerKeyArgumentCaptor.capture(), triggerArgumentCaptor.capture());
+
+        TriggerKey triggerKey = triggerKeyArgumentCaptor.getValue();
+        Trigger trigger = triggerArgumentCaptor.getValue();
+
+        assertRescheduleAfterFailure(triggerKey, trigger, work, 0);
+    }
+
+    @Test
+    void testRescheduleAfterFailure() throws Exception {
+        when(resource.getId()).thenReturn(RESOURCE_ID);
+        Work work = manager.schedule(resource);
+
+        manager.rescheduleAfterFailure(work);
+
+        verify(quartz).rescheduleJob(triggerKeyArgumentCaptor.capture(), triggerArgumentCaptor.capture());
+
+        TriggerKey triggerKey = triggerKeyArgumentCaptor.getValue();
+        Trigger trigger = triggerArgumentCaptor.getValue();
+
+        assertRescheduleAfterFailure(triggerKey, trigger, work, 1);
+    }
+
+    @Test
+    void testExists() throws SchedulerException {
+        Bridge bridge = Fixtures.createBridge();
+        manager.exists(bridge);
+
+        verify(quartz).checkExists(triggerKeyArgumentCaptor.capture());
+
+        TriggerKey triggerKey = triggerKeyArgumentCaptor.getValue();
+        assertThat(triggerKey.getName()).isEqualTo(bridge.getId());
+        assertThat(triggerKey.getGroup()).isEqualTo(bridge.getClass().getName());
+    }
+
+    private void assertRescheduleAfterFailure(TriggerKey triggerKey, Trigger trigger, Work work, long attempts) {
+        assertThat(triggerKey.getName()).isEqualTo(RESOURCE_ID);
+        assertThat(triggerKey.getGroup()).isEqualTo(work.getType());
+
+        assertThat(trigger.getKey().getName()).isEqualTo(RESOURCE_ID);
+        assertThat(trigger.getKey().getGroup()).isEqualTo(work.getType());
+        assertThat(trigger.getJobKey()).isNotNull();
+
+        JobDataMap data = trigger.getJobDataMap();
+        assertThat(data.getString(STATE_FIELD_ID)).isEqualTo(RESOURCE_ID);
+        assertThat(data.getLong(STATE_FIELD_ATTEMPTS)).isEqualTo(attempts);
         assertThat(data.get(STATE_FIELD_SUBMITTED_AT)).isNotNull();
     }
 
