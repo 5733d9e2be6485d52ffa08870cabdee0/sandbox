2d81c94361ce7b2fe1756e26cacef0a667ecf571
Michael Anstis <manstis@users.noreply.github.com>
2022-07-14 12:25:45 +0100

    MGDOBR-652: Make Managed Connector Configuration Property Names Consistent (#928)
    Co-authored-by: Michael Anstis <manstis@redhat.com>

--- dev/bin/interaction/delete-managed-connectors.sh
+++ dev/bin/interaction/delete-managed-connectors.sh
@@ -11,9 +11,9 @@
 export COS_BASE_PATH=${MANAGED_CONNECTORS_CONTROL_PLANE_URL}/api/connector_mgmt/v1
 export CONNECTORS_BASE=${COS_BASE_PATH}/kafka_connectors
 
-MC_IDS=$(curl -L --insecure --oauth2-bearer "$(ocm token)" "${CONNECTORS_BASE}" | jq -r '(now - 12*60*60 | strflocaltime("%Y-%m-%dT%H:%M:%S")) as $older  | .items[] | select(.name | startswith("ci-")) | select(.owner="openbridge_kafka_supporting") | select((.created_at | sub("-(?<d>[0-9]-)";"-0\(.d)")) <= $older) | .id')
+MANAGED_CONNECTORS_IDS=$(curl -L --insecure --oauth2-bearer "$(ocm token)" "${CONNECTORS_BASE}" | jq -r '(now - 12*60*60 | strflocaltime("%Y-%m-%dT%H:%M:%S")) as $older  | .items[] | select(.name | startswith("ci-")) | select(.owner="openbridge_kafka_supporting") | select((.created_at | sub("-(?<d>[0-9]-)";"-0\(.d)")) <= $older) | .id')
 
-for ID in $MC_IDS
+for ID in $MANAGED_CONNECTORS_IDS
 do
   curl -L --insecure --oauth2-bearer "$(ocm token)" -S -s -D /dev/stderr -XDELETE "${CONNECTORS_BASE}"/"${ID}" | jq
   echo "Deleted Managed Connector with id ${ID}"
