74c3212670eceb2f71ab8fa949a8abb68bac6364
Jacopo Rota <jacopo.r00ta@gmail.com>
2022-07-26 09:09:25 +0200

    refactoring

--- manager/src/main/resources/db/migration/0.1/V0.1.16__add_shard_router_hostname.sql
+++ manager/src/main/resources/db/migration/0.1/V0.1.16__add_shard_router_hostname.sql
@@ -1,2 +1,8 @@
 alter table SHARD
     add column "router_canonical_hostname" varchar(255);
+
+update SHARD
+    set router_canonical_hostname='${shard-router-canonical-hostname}' where id='${shard-id}';
+
+alter table SHARD
+    alter column router_canonical_hostname set NOT NULL;
