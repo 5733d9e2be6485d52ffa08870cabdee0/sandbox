dbcf565cc9259028cc6662c96183ae5d92b95182
Jacopo Rota <jacopo.r00ta@gmail.com>
2022-08-04 13:52:57 +0200

    add jira in the comments

--- manager/src/main/java/com/redhat/service/smartevents/manager/dns/KnativeBrokerPathBuilder.java
+++ manager/src/main/java/com/redhat/service/smartevents/manager/dns/KnativeBrokerPathBuilder.java
@@ -2,6 +2,7 @@ package com.redhat.service.smartevents.manager.dns;
 
 // At the moment we have to craft the endpoint as the fleet-shard/knative would do.
 // Remove this logic when https://github.com/knative/eventing/issues/6467 is fixed and we can resolve the path directly in the Ingress/Route
+// https://issues.redhat.com/browse/MGDOBR-998
 public class KnativeBrokerPathBuilder {
     public static String build(String customerId, String bridgeId) {
         return String.format("/ob-%s/ob-%s", customerId, bridgeId);
