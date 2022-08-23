7453afb39a17789ea0920f4ec1c873eb66366255
Michael Anstis <manstis@redhat.com>
2022-08-23 14:52:58 +0100

    MGDOBR-456: Improve names of BridgeDAO#findByShardIdWithReadyDependencies and ProcessorDAO#findByShardIdWithReadyDependencies. More methods.

--- manager/src/main/java/com/redhat/service/smartevents/manager/ProcessorServiceImpl.java
+++ manager/src/main/java/com/redhat/service/smartevents/manager/ProcessorServiceImpl.java
@@ -296,7 +296,7 @@ public class ProcessorServiceImpl implements ProcessorService {
 
     @Transactional
     @Override
-    public List<Processor> findByShardIdWithReadyDependencies(String shardId) {
+    public List<Processor> findByShardIdToDeployOrDelete(String shardId) {
         return processorDAO.findByShardIdToDeployOrDelete(shardId);
     }
 
