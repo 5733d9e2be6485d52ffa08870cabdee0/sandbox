8fa97ffad1a7a1e33a3237ac59723e3f6431ea1c
Alessandro Costa <alecosta.me@gmail.com>
2022-08-29 15:38:35 +0200

    MGDOBR-1018: improve ResourceNamesProviderTest

--- manager/src/main/java/com/redhat/service/smartevents/manager/providers/ResourceNamesProviderImpl.java
+++ manager/src/main/java/com/redhat/service/smartevents/manager/providers/ResourceNamesProviderImpl.java
@@ -13,7 +13,7 @@ public class ResourceNamesProviderImpl implements ResourceNamesProvider {
     public static final String PROCESSOR_SHORTNAME = "prcs";
     public static final String ERROR_TOPIC_SUFFIX = "err";
 
-    private static final String VALIDATION_REGEX = "^[a-z][a-z0-9-]{0,19}$";
+    private static final String VALIDATION_REGEX = "^[a-z][a-z0-9-]{0,16}$";
 
     @ConfigProperty(name = RESOURCE_PREFIX_PROPERTY)
     String resourcePrefix;
@@ -30,7 +30,7 @@ public class ResourceNamesProviderImpl implements ResourceNamesProvider {
 
     @Override
     public String getGlobalErrorTopicName() {
-        return String.format("%s%s", validatedResourcePrefix, "err");
+        return String.format("%s%s", validatedResourcePrefix, ERROR_TOPIC_SUFFIX);
     }
 
     @Override
