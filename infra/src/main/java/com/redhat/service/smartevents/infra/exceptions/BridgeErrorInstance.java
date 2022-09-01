e6f4b74555bc9d5796bba2fd7ade0c8454fff161
Michael Anstis <manstis@redhat.com>
2022-09-01 11:02:47 +0100

    Update OpenAPI definition.

--- infra/src/main/java/com/redhat/service/smartevents/infra/exceptions/BridgeErrorInstance.java
+++ infra/src/main/java/com/redhat/service/smartevents/infra/exceptions/BridgeErrorInstance.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Schema
 public class BridgeErrorInstance extends BridgeError {
 
@@ -19,6 +21,7 @@ public class BridgeErrorInstance extends BridgeError {
         this.uuid = UUID.randomUUID().toString();
     }
 
+    @JsonIgnore
     public String getUUID() {
         return uuid;
     }
