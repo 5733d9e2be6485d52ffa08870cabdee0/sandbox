a325c5c994099a61c4ba7a2ea5eea80aadc4c8a3
Vaibhav Jain <vaibhavjain.wiz@gmail.com>
2022-09-01 12:43:06 +0530

    MGDOBR-1036 : The Shard should read configuration from a Secret on start-up

--- dev/bin/shard-run.sh
+++ dev/bin/shard-run.sh
@@ -16,8 +16,5 @@ mvn \
   -Dquarkus.http.port=1337 \
   -Devent-bridge.logging.json=false \
   -Devent-bridge.k8s.orchestrator=minikube \
-  -Devent-bridge.istio.gateway.name=istio-ingressgateway \
-  -Devent-bridge.istio.gateway.namespace=istio-system \
-  -Devent-bridge.webhook.technical-account-id=402cf429-da04-4931-8089-e53ad452192b \
   -f "${SCRIPT_DIR_PATH}/../../shard-operator/pom.xml" \
   clean compile quarkus:dev $@
