f4dc9fc979f172f515c4abc8d3e603287230b2b8
Aakanksha Tripathi <aatripat@aatripat.remote.csb>
2022-09-28 15:30:05 +0530

    [MGDOBR-1111] : Sonarcloud constructor issue
    MGDOBR : Sonarcloud constructor issue formatted
    
    MGDOBR : Sonarcloud issue constructor formattedfiles
    
    MGDOBR : Sonarcloud issue constructor formattedfiles
    
    [MGDOBR] : Sonarcloud constructor issue
    
    [MGDOBR] : Sonarcloud constructor issue import fixed
    
    [MGDOBR] : Sonarcloud constructor issue
    
    [MGDOBR] Sonarcloud issue sorted imports
    
    Sonarcloud import fixed for constructor issue

--- infra/src/main/java/com/redhat/service/smartevents/infra/auth/OidcClientConstants.java
+++ infra/src/main/java/com/redhat/service/smartevents/infra/auth/OidcClientConstants.java
@@ -1,5 +1,9 @@
 package com.redhat.service.smartevents.infra.auth;
 
 public class OidcClientConstants {
+    private OidcClientConstants() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
     public static final String WEBHOOK_OIDC_CLIENT_NAME = "webhook-oidc-client";
 }
--- infra/src/main/java/com/redhat/service/smartevents/infra/utils/Constants.java
+++ infra/src/main/java/com/redhat/service/smartevents/infra/utils/Constants.java
@@ -1,6 +1,12 @@
 package com.redhat.service.smartevents.infra.utils;
 
+//@NoArgsConstructor(access= AccessLevel.PRIVATE)
+//@SuppressWarnings("java:S1118")
 public class Constants {
+    private Constants() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
     public static final String API_IDENTIFIER_PREFIX = "OPENBRIDGE-";
     public static final String HTTP_SCHEME = "http://";
     public static final String HTTPS_SCHEME = "https://";
