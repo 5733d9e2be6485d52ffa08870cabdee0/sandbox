af523211bd6d4401e0214058aaf8497ab4ff3d2e
Savitha Thundathil Jose <sthundat@localhost.localdomain>
2022-10-07 14:17:01 +0530

    [MGDOBR-1131]:Restructuring framework by moving common classes of integration tests into a new module

--- test-commons/src/main/java/com/redhat/service/smartevents/integration/tests/context/resolver/BridgeEndpointPathResolver.java
+++ test-commons/src/main/java/com/redhat/service/smartevents/integration/tests/context/resolver/BridgeEndpointPathResolver.java
@@ -5,19 +5,19 @@ import java.util.regex.Pattern;
 
 import com.redhat.service.smartevents.integration.tests.context.TestContext;
 
-public class BridgeEndpointAuthorityResolver implements Resolver {
+public class BridgeEndpointPathResolver implements Resolver {
 
-    private static final Pattern BRIDGE_ENDPOINT_AUTHORITY_REGEX = Pattern.compile("\\$\\{bridge\\.([^\\.]+)\\.endpoint\\.authority\\}");
+    private static final Pattern BRIDGE_ENDPOINT_PATH_REGEX = Pattern.compile("\\$\\{bridge\\.([^\\.]+)\\.endpoint\\.path\\}");
 
     public boolean match(String placeholder) {
-        return BRIDGE_ENDPOINT_AUTHORITY_REGEX.matcher(placeholder).find();
+        return BRIDGE_ENDPOINT_PATH_REGEX.matcher(placeholder).find();
     }
 
     public String replace(String content, TestContext context) {
-        Matcher matcher = BRIDGE_ENDPOINT_AUTHORITY_REGEX.matcher(content);
+        Matcher matcher = BRIDGE_ENDPOINT_PATH_REGEX.matcher(content);
         return matcher.replaceAll(match -> {
             String testBridgeName = match.group(1);
-            return context.getBridge(testBridgeName).getEndPointAuthority();
+            return context.getBridge(testBridgeName).getEndPointPath();
         });
     }
 }
