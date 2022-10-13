dd6253eb6e41759af74f064b28eccf8a05b161c9
Rob Blake <rblake@redhat.com>
2022-10-13 15:51:39 +0100

    [MGDOBR-1154] Add support for prod branch to the deploy bot (#1233)
    * [MGDOBR-1154] Add support for prod branch to the deploy bot
    
    * [MGDOBR-1154] Add support for prod branch to the deploy bot
    
    * [MGDOBR-1154] Add clarity around the need to deploy to dev,stable first, before promoting to prod

--- .github/scripts/rebase-and-deploy-to-env.sh
+++ .github/scripts/rebase-and-deploy-to-env.sh
@@ -1,14 +1,14 @@
 # Env vars parameters:
-# TARGET_BRANCH: dev, stable
+# TARGET_BRANCH: dev, stable, prod
 # SHA_COMMIT: the sha of the commit to rebase
 # AUTHOR: The user
 # GITHUB_TOKEN: The token
 
-allowed_branch_values=("dev" "stable")
+allowed_branch_values=("dev" "stable" "prod")
 
 # Check for valid choices
 if ! grep -q "$TARGET_BRANCH" <<< "${allowed_branch_values[@]}"; then
-    printf "\U274C $TARGET_BRANCH is not a valid choice. Valid choices are dev and stable."
+    printf "\U274C $TARGET_BRANCH is not a valid choice. Valid choices are dev,stable and prod."
     exit 0
 fi
 
@@ -19,6 +19,7 @@ cd $UPSTREAM_REPO_LOCATION
 # peek branches 
 git checkout --track origin/dev 1>&2
 git checkout --track origin/stable 1>&2
+git checkout --track origin/prod 1>&2
 git checkout main 1>&2
 
 # If the deployment targets `stable`, then the feature must be on dev first.
@@ -29,6 +30,15 @@ if [[ "$TARGET_BRANCH" == "stable" ]]; then
   fi
 fi
 
+# If the deployment targets `prod`, then the feature must be on stable first.
+if [[ "$TARGET_BRANCH" == "prod" ]]; then
+  if [ $(git branch --contains $SHA_COMMIT | grep -c "stable") -eq 0 ]; then
+    printf "\U274C In order to deploy to prod branch the feature must be on stable branch first. Please deploy the feature to dev and then to stable, check that everything is running fine and then, finally, promote it to prod."
+    exit 0
+  fi
+fi
+
+
 # If the commit is already on TARGET_BRANCH, the feature has been already merged and deployed.
 if [ $(git branch --contains $SHA_COMMIT | grep -c "$TARGET_BRANCH") -ne 0 ]; then
   printf "\U2705 $SHA_COMMIT is already on $TARGET_BRANCH branch!"
@@ -36,7 +46,7 @@ else
   git checkout $TARGET_BRANCH 1>&2
   git rebase $SHA_COMMIT 1>&2
   git push origin $TARGET_BRANCH 1>&2
-  printf "\U2705 $SHA_COMMIT was not on $TARGET_BRANCH branch. $TARGET_BRANCH branch has been rebased and pushed to the upstream repository. The deployment is on the way!"
+  printf "\U2705 $SHA_COMMIT was not on $TARGET_BRANCH branch. $TARGET_BRANCH branch has been rebased and pushed to the upstream repository. Check ArgoCD for the deployment status!"
 fi
 
 rm -rf $UPSTREAM_REPO_LOCATION
