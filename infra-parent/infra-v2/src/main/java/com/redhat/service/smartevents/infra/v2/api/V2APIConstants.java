8c2ce6ce60f618ad7d7bc7b9fe7d0884afc0fb69
Jacopo Rota <jacopo.r00ta@gmail.com>
2022-11-09 16:17:28 +0100

    add infra and manager v2 modules

---
+++ infra-parent/infra-v2/src/main/java/com/redhat/service/smartevents/infra/v2/api/V2APIConstants.java
@@ -0,0 +1,32 @@
+package com.redhat.service.smartevents.infra.v2.api;
+
+import com.redhat.service.smartevents.infra.core.api.APIConstants;
+
+/**
+ * Some constants used for URL construction for the API
+ */
+public class V2APIConstants {
+
+    /**
+     * Base Path for the Smart Events application API v2.
+     */
+    public static final String V2_ROOT = APIConstants.ROOT + "/v2";
+
+    /**
+     * Base Path for the Cloud Providers API
+     */
+    public static final String V1_CLOUD_PROVIDERS_BASE_PATH = V2_ROOT + "/cloud_providers";
+
+    /**
+     * Base Path for the user-facing API
+     */
+    public static final String V1_USER_API_BASE_PATH = V2_ROOT + "/bridges/";
+
+    /**
+     * Base Path for Shard facing API.
+     */
+    public static final String V1_SHARD_API_BASE_PATH = V2_ROOT + "/shard/bridges/";
+
+    private V2APIConstants() {
+    }
+}
