719263cd810407e4db33389ddcd99e146fbf610e
Alessandro Costa <alecosta.me@gmail.com>
2022-11-18 10:41:32 +0100

    MGDOBR-1221: add describe nodes requests

--- dev/bin/camel-k-installer.sh
+++ dev/bin/camel-k-installer.sh
@@ -34,5 +34,13 @@ chmod +x "$cli_file_name"
 
 echo "Using Camel CLI v${camel_k_version} for ${host_os}"
 
+echo "---- K8S NODE DESCRIPTION - PRE INSTALL ----"
+kubectl describe nodes
+echo "--------------------------------------------"
+
 kubectl create namespace camel-k --dry-run=client -o yaml | kubectl apply -f -
-"./${cli_file_name}" install -n camel-k --global --force -w $@
+"./${cli_file_name}" install -n camel-k --global --force -w -V $@
+
+echo "---- K8S NODE DESCRIPTION - POST INSTALL ---"
+kubectl describe nodes
+echo "--------------------------------------------"
