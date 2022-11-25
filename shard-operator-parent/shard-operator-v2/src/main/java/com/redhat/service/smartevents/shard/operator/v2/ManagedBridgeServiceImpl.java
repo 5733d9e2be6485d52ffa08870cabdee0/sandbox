9719978ecbfb2d16b9a1442990b7ab7f3af903bd
Rob Blake <rblake@redhat.com>
2022-11-25 09:28:32 +0000

    [MGDOBR-1268] Implemented the v2 namespace strategy for the data plane (#1348)
    * [MGDOBR-1268] Implemented the v2 namespace strategy
    Minor update to the namespace strategy for v2 of the data plane which provisions a namespace-per-bridge rather than a namespace-per-customer
    
    * [MGDOBR-1268] Removed codesmell warnings by using containsEntry assertion from AssertJ for map entries

---
+++ shard-operator-parent/shard-operator-v2/src/main/java/com/redhat/service/smartevents/shard/operator/v2/ManagedBridgeServiceImpl.java
@@ -0,0 +1,35 @@
+package com.redhat.service.smartevents.shard.operator.v2;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import com.redhat.service.smartevents.shard.operator.v2.providers.NamespaceProvider;
+import com.redhat.service.smartevents.shard.operator.v2.resources.ManagedBridge;
+
+import io.fabric8.kubernetes.api.model.Namespace;
+
+@ApplicationScoped
+public class ManagedBridgeServiceImpl implements ManagedBridgeService {
+
+    @Inject
+    private NamespaceProvider namespaceProvider;
+
+    @Override
+    public void createManagedBridgeResources(ManagedBridge managedBridge) {
+        Namespace namespace = namespaceProvider.fetchOrCreateNamespace(managedBridge);
+
+        /*
+         * Pull in the rest of the logic from BridgeIngressserviceimpl to create the other resources
+         */
+    }
+
+    @Override
+    public void deleteManagedBridgeResources(ManagedBridge managedBridge) {
+
+        /*
+         * Pull in the rest of the logic from BridgeIngressServiceImpl to delete the other resources
+         */
+
+        namespaceProvider.deleteNamespace(managedBridge);
+    }
+}
