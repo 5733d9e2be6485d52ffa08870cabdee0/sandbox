a74a3b60797e7debaa29ea02c523741a3abb0661
Alessandro Costa <alecosta.me@gmail.com>
2022-11-29 11:03:40 +0100

    MGDOBR-1221: remove Kourier and eventing additions

--- dev/bin/knative-installer.sh
+++ dev/bin/knative-installer.sh
@@ -26,18 +26,6 @@ kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1
 header_text "Waiting for Knative Serving Core to become ready"
 kubectl wait deployment --all --timeout=900s --for=condition=Available -n knative-serving
 
-header_text "Initializing Kourier"
-kubectl apply -f https://github.com/knative/net-kourier/releases/download/knative-v1.5.0/kourier.yaml
-
-header_text "Waiting for Kourier to become ready"
-kubectl wait deployment --all --timeout=900s --for=condition=Available -n kourier-system
-
-header_text "Patching Knative Serving config to use Kourier"
-kubectl patch configmap/config-network \
-  --namespace knative-serving \
-  --type merge \
-  --patch '{"data":{"ingress-class":"kourier.ingress.networking.knative.dev"}}'
-
 header_text "Knative Eventing Kafka - Installer"
 header_text "Initializing Knative Eventing Core APIs"
 kubectl apply -f https://github.com/knative/eventing/releases/download/knative-v1.5.6/eventing-crds.yaml
@@ -51,16 +39,4 @@ kubectl apply -f https://github.com/knative-sandbox/eventing-kafka-broker/releas
 kubectl apply -f https://github.com/knative-sandbox/eventing-kafka-broker/releases/download/knative-v1.5.8/eventing-kafka-broker.yaml
 
 header_text "Waiting for Knative Eventing Kafka to become ready"
-kubectl wait deployment --all --timeout=900s --for=condition=Available -n knative-eventing
-
-header_text "Initializing Knative Eventing in-memory channel"
-kubectl apply -f https://github.com/knative/eventing/releases/download/knative-v1.5.6/in-memory-channel.yaml
-
-header_text "Waiting for Knative Eventing in-memory channel to become ready"
-kubectl wait deployment --all --timeout=900s --for=condition=Available -n knative-eventing
-
-header_text "Initializing Knative Eventing multi-tenant channel-based broker"
-kubectl apply -f https://github.com/knative/eventing/releases/download/knative-v1.5.6/mt-channel-broker.yaml
-
-header_text "Waiting for Knative Eventing multi-tenant channel-based broker to become ready"
 kubectl wait deployment --all --timeout=900s --for=condition=Available -n knative-eventing
