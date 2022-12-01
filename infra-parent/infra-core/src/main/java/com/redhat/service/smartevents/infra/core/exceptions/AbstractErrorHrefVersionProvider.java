0112bef8ec98b12195b94d317a6e2a1178867d65
Jacopo Rota <jacopo.r00ta@gmail.com>
2022-12-01 23:05:49 +0100

    refactoring

--- infra-parent/infra-core/src/main/java/com/redhat/service/smartevents/infra/core/exceptions/AbstractErrorHrefVersionProvider.java
+++ infra-parent/infra-core/src/main/java/com/redhat/service/smartevents/infra/core/exceptions/AbstractErrorHrefVersionProvider.java
@@ -5,15 +5,19 @@ import java.util.Arrays;
 import javax.validation.ConstraintViolation;
 
 public abstract class AbstractErrorHrefVersionProvider implements ErrorHrefVersionProvider {
+
+    @Override
     public boolean accepts(Throwable e) {
         return Arrays.stream(e.getStackTrace()).anyMatch(x -> x.getClassName().matches(getPackageRegexMatch()));
     }
 
+    @Override
     public boolean accepts(ConstraintViolation<?> cv) {
         return cv.getRootBeanClass().getName().matches(getPackageRegexMatch());
 
     }
 
+    @Override
     public String buildHref(String id) {
         return getBaseHref() + id;
     }
