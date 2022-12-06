6b3bbffabc7f029f911a730ef612476de4e8a8e6
Vaibhav Jain <vajain@redhat.com>
2022-12-06 16:29:25 +0530

    fix formatting

--- shard-operator-parent/shard-operator-v1/src/test/java/com/redhat/service/smartevents/shard/operator/v1/networking/OpenshiftNetworkingServiceTest.java
+++ shard-operator-parent/shard-operator-v1/src/test/java/com/redhat/service/smartevents/shard/operator/v1/networking/OpenshiftNetworkingServiceTest.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import javax.inject.Inject;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.redhat.service.smartevents.shard.operator.core.providers.GlobalConfigurationsConstants;
@@ -19,6 +18,8 @@ import io.fabric8.kubernetes.client.utils.KubernetesResourceUtil;
 import io.fabric8.openshift.client.OpenShiftClient;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.kubernetes.client.WithOpenShiftTestServer;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @QuarkusTest
 @WithOpenShiftTestServer
@@ -34,14 +35,14 @@ public class OpenshiftNetworkingServiceTest {
     public void TestOpenshiftNetworkingService() {
         OpenshiftNetworkingService openshiftNetworkingService = new OpenshiftNetworkingService(client, new TemplateProviderImpl(), istioGatewayProvider);
         Secret secret = new Secret();
-        Map<String, String> Secretdata = new HashMap<>();
-        Secretdata.put(GlobalConfigurationsConstants.TLS_CERTIFICATE_SECRET, "VExTX0NFUlRJRklDQVRF");
-        Secretdata.put(GlobalConfigurationsConstants.TLS_KEY_SECRET, "VExTX0tFWV9TRUNSRVQ=");
-        secret.setData(Secretdata);
+        Map<String, String> secretData = new HashMap<>();
+        secretData.put(GlobalConfigurationsConstants.TLS_CERTIFICATE_SECRET, "VExTX0NFUlRJRklDQVRF");
+        secretData.put(GlobalConfigurationsConstants.TLS_KEY_SECRET, "VExTX0tFWV9TRUNSRVQ=");
+        secret.setData(secretData);
 
         NetworkResource networkResource = openshiftNetworkingService.fetchOrCreateBrokerNetworkIngress(buildRouteIngress(), secret, "testPath");
-        Assertions.assertThat(networkResource.isReady()).isFalse();
-        Assertions.assertThat(networkResource.getEndpoint()).isNull();
+        assertThat(networkResource.isReady()).isFalse();
+        assertThat(networkResource.getEndpoint()).isNull();
     }
 
     private BridgeIngress buildRouteIngress() {
