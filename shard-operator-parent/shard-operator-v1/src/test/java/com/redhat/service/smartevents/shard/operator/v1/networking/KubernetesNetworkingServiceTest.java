126dcc59f850e31ebbd6799716ad8be8e782d71e
rpancham <rpancham@redhat.com>
2022-12-06 14:16:39 +0530

    Fix Formatting

--- shard-operator-parent/shard-operator-v1/src/test/java/com/redhat/service/smartevents/shard/operator/v1/networking/KubernetesNetworkingServiceTest.java
+++ shard-operator-parent/shard-operator-v1/src/test/java/com/redhat/service/smartevents/shard/operator/v1/networking/KubernetesNetworkingServiceTest.java
@@ -1,17 +1,19 @@
 package com.redhat.service.smartevents.shard.operator.v1.networking;
 
-        import com.redhat.service.smartevents.shard.operator.core.providers.IstioGatewayProvider;
-        import com.redhat.service.smartevents.shard.operator.v1.TestSupport;
-        import com.redhat.service.smartevents.shard.operator.v1.providers.TemplateProvider;
-        import com.redhat.service.smartevents.shard.operator.v1.resources.BridgeIngress;
-        import io.fabric8.kubernetes.client.KubernetesClient;
-        import io.fabric8.kubernetes.client.utils.KubernetesResourceUtil;
-        import io.quarkus.test.junit.QuarkusTest;
-        import io.quarkus.test.kubernetes.client.WithOpenShiftTestServer;
-        import org.assertj.core.api.Assertions;
-        import org.junit.jupiter.api.Test;
+import javax.inject.Inject;
 
-        import javax.inject.Inject;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.redhat.service.smartevents.shard.operator.core.providers.IstioGatewayProvider;
+import com.redhat.service.smartevents.shard.operator.v1.TestSupport;
+import com.redhat.service.smartevents.shard.operator.v1.providers.TemplateProvider;
+import com.redhat.service.smartevents.shard.operator.v1.resources.BridgeIngress;
+
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.utils.KubernetesResourceUtil;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.kubernetes.client.WithOpenShiftTestServer;
 
 @QuarkusTest
 @WithOpenShiftTestServer
--- shard-operator-parent/shard-operator-v1/src/test/java/com/redhat/service/smartevents/shard/operator/v1/networking/OpenshiftNetworkingServiceTest.java
+++ shard-operator-parent/shard-operator-v1/src/test/java/com/redhat/service/smartevents/shard/operator/v1/networking/OpenshiftNetworkingServiceTest.java
@@ -1,23 +1,25 @@
 package com.redhat.service.smartevents.shard.operator.v1.networking;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import com.redhat.service.smartevents.shard.operator.core.providers.GlobalConfigurationsConstants;
 import com.redhat.service.smartevents.shard.operator.core.providers.IstioGatewayProvider;
 import com.redhat.service.smartevents.shard.operator.v1.TestSupport;
 import com.redhat.service.smartevents.shard.operator.v1.providers.TemplateProvider;
 import com.redhat.service.smartevents.shard.operator.v1.resources.BridgeIngress;
-//import io.fabric8.kubernetes.client.KubernetesClient;
+
+// import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.api.model.Secret;
 import io.fabric8.kubernetes.client.utils.KubernetesResourceUtil;
 import io.fabric8.openshift.client.OpenShiftClient;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.kubernetes.client.WithOpenShiftTestServer;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import javax.inject.Inject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -37,14 +39,13 @@ public class OpenshiftNetworkingServiceTest {
     @Test
     public void TestOpenshiftNetworkingService() {
         OpenshiftNetworkingService openshiftNetworkingService = new OpenshiftNetworkingService(client, templateProvider, istioGatewayProvider);
-        Secret secret=new Secret();
-        Map<String, String> Secretdata=new HashMap<>();
-        Secretdata.put(GlobalConfigurationsConstants.TLS_CERTIFICATE_SECRET,"VExTX0NFUlRJRklDQVRF");
-        Secretdata.put(GlobalConfigurationsConstants.TLS_KEY_SECRET,"VExTX0tFWV9TRUNSRVQ=");
+        Secret secret = new Secret();
+        Map<String, String> Secretdata = new HashMap<>();
+        Secretdata.put(GlobalConfigurationsConstants.TLS_CERTIFICATE_SECRET, "VExTX0NFUlRJRklDQVRF");
+        Secretdata.put(GlobalConfigurationsConstants.TLS_KEY_SECRET, "VExTX0tFWV9TRUNSRVQ=");
         secret.setData(Secretdata);
 
-
-       NetworkResource networkResource = openshiftNetworkingService.fetchOrCreateBrokerNetworkIngress(buildRouteIngress(), secret, "testPath");
+        NetworkResource networkResource = openshiftNetworkingService.fetchOrCreateBrokerNetworkIngress(buildRouteIngress(), secret, "testPath");
         Assertions.assertThat(networkResource.isReady()).isFalse();
         Assertions.assertThat(networkResource.getEndpoint()).isNull();
     }
