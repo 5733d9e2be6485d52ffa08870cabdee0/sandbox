9ec4186c72b010bf6e21c63aaeb01ba4e4d3e59c
Vaibhav Jain <vajain@redhat.com>
2022-12-12 14:18:02 +0530

    Fix formatting

--- shard-operator-parent/shard-operator-core/src/test/java/com/redhat/service/smartevents/shard/operator/core/utils/StringUtilsTest.java
+++ shard-operator-parent/shard-operator-core/src/test/java/com/redhat/service/smartevents/shard/operator/core/utils/StringUtilsTest.java
@@ -1,7 +1,7 @@
 package com.redhat.service.smartevents.shard.operator.core.utils;
 
-import org.junit.jupiter.api.Test;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class StringUtilsTest {
 
