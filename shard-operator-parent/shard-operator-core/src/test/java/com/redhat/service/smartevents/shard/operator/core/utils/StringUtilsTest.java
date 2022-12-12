ff6a67c17c2b96befd57478f693806d2ebef4097
rpancham <rpancham@redhat.com>
2022-12-12 13:14:52 +0530

    Fix testcases

--- shard-operator-parent/shard-operator-core/src/test/java/com/redhat/service/smartevents/shard/operator/core/utils/StringUtilsTest.java
+++ shard-operator-parent/shard-operator-core/src/test/java/com/redhat/service/smartevents/shard/operator/core/utils/StringUtilsTest.java
@@ -1,33 +1,27 @@
 package com.redhat.service.smartevents.shard.operator.core.utils;
 
-
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 public class StringUtilsTest {
 
     @Test
-    public void TestStringIsNullOrEmpty(){
-
+    public void TestStringIsNullOrEmpty() {
         boolean result = StringUtils.stringIsNullOrEmpty(null);
-        Assertions.assertTrue(result);
-
+        Assertions.assertThat(Boolean.parseBoolean(String.valueOf(result))).isTrue();
 
         result = StringUtils.stringIsNullOrEmpty("");
-        Assertions.assertTrue(result);
-
+        Assertions.assertThat(Boolean.parseBoolean(String.valueOf(result))).isTrue();
 
     }
+
     @Test
-    public void TestemptyToNull(){
+    public void TestemptyToNull() {
         String testString = null;
         testString = StringUtils.emptyToNull(null);
-         Assertions.assertFalse(Boolean.parseBoolean(testString));
-
-
+        Assertions.assertThat(Boolean.parseBoolean(testString)).isFalse();
 
     }
 
 }
-
-
