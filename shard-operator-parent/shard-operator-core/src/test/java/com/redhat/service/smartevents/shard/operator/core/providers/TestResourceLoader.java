07742a0de4682e0bc55d303b0c87063a1352173a
Vaibhav Jain <vajain@redhat.com>
2022-12-16 14:17:21 +0530

    fix testcase

--- shard-operator-parent/shard-operator-core/src/test/java/com/redhat/service/smartevents/shard/operator/core/providers/TestResourceLoader.java
+++ shard-operator-parent/shard-operator-core/src/test/java/com/redhat/service/smartevents/shard/operator/core/providers/TestResourceLoader.java
@@ -1,6 +1,10 @@
 package com.redhat.service.smartevents.shard.operator.core.providers;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.client.utils.Serialization;
 
 public class TestResourceLoader {
 
@@ -8,7 +12,14 @@ public class TestResourceLoader {
     private static final String TEST_DEPLOYMENT_PATH = TEMPLATES_DIR + "/test_deployment.yaml";
 
     public static Deployment loadTestDeployment() {
-        TemplateProviderImpl templateProvider = new TemplateProviderImpl();
-        return templateProvider.loadYaml(Deployment.class, TEST_DEPLOYMENT_PATH);
+        return loadYaml(Deployment.class, TEST_DEPLOYMENT_PATH);
+    }
+
+    private static <T> T loadYaml(Class<T> clazz, String yaml) {
+        try (InputStream is = TemplateProviderImpl.class.getResourceAsStream(yaml)) {
+            return Serialization.unmarshal(is, clazz);
+        } catch (IOException ex) {
+            throw new IllegalStateException("Cannot find yaml on classpath: " + yaml);
+        }
     }
 }
