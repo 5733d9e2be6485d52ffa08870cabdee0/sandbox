2770ce046e28700e30dcdedab245206bd2b2672d
Jacopo Rota <jacopo.r00ta@gmail.com>
2022-12-21 23:31:00 +0100

    minimalize knative istio installer

--- dev/bin/knative-installer.sh
+++ dev/bin/knative-installer.sh
@@ -27,30 +27,9 @@ header_text "Waiting for Knative Serving Core to become ready"
 kubectl wait deployment --all --timeout=900s --for=condition=Available -n knative-serving
 
 header_text "Installing Istio Configuration for Knative Serving"
-# This command _should_ only install the CRDS first using the label selector. However it seems to create other resources
-# so we add the "|| true" statement on the end to force a 0 exit code and not break the script. We then run the apply
-# command again and verify that all resources have come up correctly so this approach is ~kinda safe.
-kubectl apply -l knative.dev/crd-install=true -f https://github.com/knative/net-istio/releases/download/knative-v1.5.0/istio.yaml || true
-kubectl apply -f https://github.com/knative/net-istio/releases/download/knative-v1.5.0/istio.yaml
-
-# We running in local dev, we don't need multiple replicas or HPA for the Istio Components
-kubectl scale --replicas=1 deployment istio-ingressgateway -n istio-system
-kubectl delete hpa istiod -n istio-system --ignore-not-found=true
-kubectl scale --replicas=1 deployment istiod -n istio-system
-
-header_text "Waiting for all Istio resources to become ready..."
-kubectl wait deployment --all --timeout=900s --for=condition=Available -n istio-system
 kubectl apply -f https://github.com/knative/net-istio/releases/download/knative-v1.5.0/net-istio.yaml
 kubectl wait deployment --all --timeout=900s --for=condition=Available -n knative-serving
 kubectl label namespace knative-serving istio-injection=enabled --overwrite
-
-# Sets the domain name configuration for all KService services to be exposed as cluster private. We do not need to
-# expose Kservice to the outside world.
-# @see https://knative.dev/docs/serving/services/private-services/
-kubectl patch configmap/config-domain \
-      --namespace knative-serving \
-      --type merge \
-      --patch '{"data":{"svc.cluster.local":""}}'
 
 header_text "Knative Eventing Kafka - Installer"
 header_text "Initializing Knative Eventing Core APIs"
