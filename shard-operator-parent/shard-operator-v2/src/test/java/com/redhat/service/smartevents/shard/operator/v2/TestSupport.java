ee95e9ca24d80f520795b70652726a857255164c
Luca Molteni <volothamp_957683@icloud.com>
2023-01-10 16:14:24 +0100

    Update shard-operator-parent/shard-operator-v2/src/test/java/com/redhat/service/smartevents/shard/operator/v2/TestSupport.java
    Co-authored-by: Jacopo Rota <jacopo.r00ta@gmail.com>

--- shard-operator-parent/shard-operator-v2/src/test/java/com/redhat/service/smartevents/shard/operator/v2/TestSupport.java
+++ shard-operator-parent/shard-operator-v2/src/test/java/com/redhat/service/smartevents/shard/operator/v2/TestSupport.java
@@ -6,12 +6,5 @@ public class TestSupport {
     public static final String BRIDGE_ID = "my-id";
     public static final String PROCESSOR_ID = "my-processor-id";
     public static final String PROCESSOR_NAME = "my-processor-name";
-    public static final String KAFKA_BOOTSTRAP_SERVERS = "mytestkafka:9092";
-    public static final String KAFKA_CLIENT_ID = "client-id";
-    public static final String KAFKA_CLIENT_SECRET = "testsecret";
-    public static final String KAFKA_SECURITY_PROTOCOL = "PLAINTEXT";
-    public static final String KAFKA_SASL_MECHANISM = "PLAIN";
-    public static final String KAFKA_TOPIC = "ob-my-id";
-    public static final String KAFKA_ERROR_TOPIC = "ob-my-id-errors";
 
 }
