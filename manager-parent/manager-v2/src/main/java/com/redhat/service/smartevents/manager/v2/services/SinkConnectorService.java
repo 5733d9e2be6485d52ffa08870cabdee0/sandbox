3891c99e46ba3cda8a8653513df905f10a7edcee
Jacopo Rota <jacopo.r00ta@gmail.com>
2023-01-16 09:36:18 +0100

    refactoring

--- manager-parent/manager-v2/src/main/java/com/redhat/service/smartevents/manager/v2/services/SinkConnectorService.java
+++
@@ -1,5 +0,0 @@
-package com.redhat.service.smartevents.manager.v2.services;
-
-public interface SinkConnectorService {
-
-}
--- manager-parent/manager-v2/src/main/java/com/redhat/service/smartevents/manager/v2/services/SinkConnectorServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.redhat.service.smartevents.manager.v2.services;
-
-import javax.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class SinkConnectorServiceImpl implements SinkConnectorService {
-
-}
