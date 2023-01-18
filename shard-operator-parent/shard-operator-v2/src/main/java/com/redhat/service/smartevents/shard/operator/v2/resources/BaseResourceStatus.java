6720e66f4fae2137df5cea1aa9aaa6cd726b0983
Michael Anstis <manstis@redhat.com>
2023-01-18 15:24:41 +0000

    Remove legacy updateErrorStatus(..) support from ManagedProcessorController.

--- shard-operator-parent/shard-operator-v2/src/main/java/com/redhat/service/smartevents/shard/operator/v2/resources/BaseResourceStatus.java
+++ shard-operator-parent/shard-operator-v2/src/main/java/com/redhat/service/smartevents/shard/operator/v2/resources/BaseResourceStatus.java
@@ -3,8 +3,6 @@ package com.redhat.service.smartevents.shard.operator.v2.resources;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.redhat.service.smartevents.infra.core.exceptions.BridgeError;
-import com.redhat.service.smartevents.infra.v2.api.models.DefaultConditions;
 import com.redhat.service.smartevents.shard.operator.core.resources.Condition;
 import com.redhat.service.smartevents.shard.operator.core.resources.ConditionStatus;
 import com.redhat.service.smartevents.shard.operator.core.resources.CustomResourceStatus;
@@ -18,14 +16,6 @@ abstract class BaseResourceStatus extends CustomResourceStatus {
     @JsonIgnore
     public final boolean isReady() {
         return getConditions().stream().allMatch(c -> ConditionStatus.True.equals(c.getStatus()));
-    }
-
-    @JsonIgnore
-    public final void setStatusFromBridgeError(BridgeError bridgeError) {
-        markConditionFailed(DefaultConditions.CP_DATA_PLANE_READY_NAME,
-                bridgeError.getReason(),
-                bridgeError.getReason(),
-                bridgeError.getCode());
     }
 
 }
