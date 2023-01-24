3136b5afb1425406158477fdef6427640f8b1fef
Jacopo Rota <jacopo.r00ta@gmail.com>
2023-01-24 16:22:25 +0100

    update deploy bot

--- .github/scripts/rebase-and-deploy-to-env.sh
+++ .github/scripts/rebase-and-deploy-to-env.sh
@@ -1,14 +1,14 @@
 # Env vars parameters:
-# TARGET_BRANCH: dev, stable, prod
+# TARGET_BRANCH: dev, prod
 # SHA_COMMIT: the sha of the commit to rebase
 # AUTHOR: The user
 # GITHUB_TOKEN: The token
 
-allowed_branch_values=("dev" "stable" "prod")
+allowed_branch_values=("dev" "prod")
 
 # Check for valid choices
 if ! grep -q "$TARGET_BRANCH" <<< "${allowed_branch_values[@]}"; then
-    printf "\U274C $TARGET_BRANCH is not a valid choice. Valid choices are dev,stable and prod."
+    printf "\U274C $TARGET_BRANCH is not a valid choice. Valid choices are dev and prod."
     exit 0
 fi
 
@@ -18,22 +18,14 @@ git clone https://$AUTHOR:$GITHUB_TOKEN@github.com/5733d9e2be6485d52ffa08870cabd
 cd $UPSTREAM_REPO_LOCATION
 # peek branches 
 git checkout --track origin/dev 1>&2
-git checkout --track origin/stable 1>&2
 git checkout --track origin/prod 1>&2
 git checkout main 1>&2
 
-# If the deployment targets `stable`, then the feature must be on dev first.
-if [[ "$TARGET_BRANCH" == "stable" ]]; then
-  if [ $(git branch --contains $SHA_COMMIT | grep -c "dev") -eq 0 ]; then
-    printf "\U274C In order to deploy to stable branch the feature must be on dev branch first. Please deploy it there first, check that everything is running fine and then promote it to stable."
-    exit 0
-  fi
-fi
 
-# If the deployment targets `prod`, then the feature must be on stable first.
+# If the deployment targets `prod`, then the feature must be on dev first.
 if [[ "$TARGET_BRANCH" == "prod" ]]; then
-  if [ $(git branch --contains $SHA_COMMIT | grep -c "stable") -eq 0 ]; then
-    printf "\U274C In order to deploy to prod branch the feature must be on stable branch first. Please deploy the feature to dev and then to stable, check that everything is running fine and then, finally, promote it to prod."
+  if [ $(git branch --contains $SHA_COMMIT | grep -c "dev") -eq 0 ]; then
+    printf "\U274C In order to deploy to prod branch the feature must be on dev branch first. Please deploy the feature to dev, check that everything is running fine and then, finally, promote it to prod."
     exit 0
   fi
 fi
